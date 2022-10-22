3a9389d80e3f05b75b7589d946606d11aff6f599
Rapuška <mikalajsiatkouski@gmail.com>
2022-10-23 00:08:39 +0300

    Создан EntitasBootstrapper

---
+++ src/EcoFarm/Assets/Code/ECS/Features/AllSystems.cs
@@ -0,0 +1,16 @@
+﻿namespace Code.ECS.Features
+{
+	public sealed class AllSystems : Feature
+	{
+		public AllSystems(Contexts contexts) : base(nameof(AllSystems))
+		{
+			
+		}
+
+		public void Tick()
+		{
+			Execute();
+			Cleanup();
+		}
+	}
+}
---
+++ src/EcoFarm/Assets/Code/Unity/EntitasBootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using Code.ECS.Features;
+using UnityEngine;
+
+namespace Code.Unity
+{
+	public class EntitasBootstrapper : MonoBehaviour
+	{
+		private AllSystems _systems;
+
+		private void Start()
+		{
+			var contexts = Contexts.sharedInstance;
+
+			_systems = new AllSystems(contexts);
+
+			_systems.Initialize();
+		}
+
+		private void Update() => _systems.Tick();
+
+		private void OnDestroy() => _systems.TearDown();
+	}
+}
