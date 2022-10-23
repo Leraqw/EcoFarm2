f43eb0c0d033ce9a7a9ca97b9bebbbbe94335d7b
Rapuška <mikalajsiatkouski@gmail.com>
2022-10-23 13:39:52 +0300

    Добавлен класс представляющий собой все сервисы

---
+++ src/EcoFarm/Assets/Code/Services/UnityImplementations/UnityAllResources.cs
@@ -0,0 +1,14 @@
+﻿using Code.Services.Interfaces;
+using UnityEngine;
+
+namespace Code.Services.UnityImplementations
+{
+	public class UnityAllResources : IAllServices
+	{
+		private readonly IResourcesLoadService _resourceLoader;
+
+		public UnityAllResources() => _resourceLoader = new UnityResourceLoadService();
+
+		GameObject IResourcesLoadService.LoadGameObject(string path) => _resourceLoader.LoadGameObject(path);
+	}
+}
