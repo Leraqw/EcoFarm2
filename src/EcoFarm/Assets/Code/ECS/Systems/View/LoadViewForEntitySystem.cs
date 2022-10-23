d52d0ac44fb737557bfa85a8f1238fa0a43292d8
Rapuška <mikalajsiatkouski@gmail.com>
2022-10-23 14:20:17 +0300

    created LoadViewForEntitySystem

---
+++ src/EcoFarm/Assets/Code/ECS/Systems/View/LoadViewForEntitySystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Entitas;
+
+namespace Code.ECS.Systems.View
+{
+	public sealed class LoadViewForEntitySystem : ReactiveSystem<GameEntity>
+	{
+		public LoadViewForEntitySystem(Contexts contexts)
+			: base(contexts.game) { }
+
+		protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
+			=> context.CreateCollector(GameMatcher.RequireGameObject);
+
+		protected override bool Filter(GameEntity entity)
+			=> entity.hasGameObject == false;
+
+		protected override void Execute(List<GameEntity> entites)
+		{
+			foreach (var e in entites)
+			{
+				
+			}
+		}
+	}
+}
