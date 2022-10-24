b834ea8448214b1d4e107585523a7dd2d6b14843
Rapuška <mikalajsiatkouski@gmail.com>
2022-10-24 13:50:44 +0300

    Spawn Multiple Trees

--- src/EcoFarm/Assets/Code/ECS/Systems/SpawnTreesSystem.cs
+++ src/EcoFarm/Assets/Code/ECS/Systems/SpawnTreesSystem.cs
@@ -6,19 +6,24 @@ using UnityEngine;
 
 namespace Code.ECS.Systems
 {
-	public sealed class SpawnTreeSystem : IInitializeSystem
+	public sealed class SpawnTreesSystem : IInitializeSystem
 	{
 		private readonly Contexts _contexts;
 
-		public SpawnTreeSystem(Contexts contexts) => _contexts = contexts;
+		public SpawnTreesSystem(Contexts contexts) => _contexts = contexts;
 
-		private Vector2 TreeSpawnPosition => SceneObjectsService.TreeSpawnPositions.First().position;
 		private ISceneObjectsService SceneObjectsService => _contexts.services.sceneObjectsService.Value;
 
 		public void Initialize()
+			=> SceneObjectsService
+			   .TreeSpawnPositions
+			   .Select((t) => t.position)
+			   .ForEach(SpawnOnPosition);
+
+		private void SpawnOnPosition(Vector3 position)
 			=> _contexts.game.CreateEntity()
 			            .Do((e) => e.isTree = true)
 			            .Do((e) => e.AddRequireView("Trees/Prefabs/Tree"))
-			            .Do((e) => e.AddSpawnPosition(TreeSpawnPosition));
+			            .Do((e) => e.AddSpawnPosition(position));
 	}
 }
