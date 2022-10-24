777bda001c5e30864284364e023d6bee3d6f02ae
Rapuška <mikalajsiatkouski@gmail.com>
2022-10-24 20:43:31 +0300

    fixed typo

---
+++ src/EcoFarm/Assets/Code/ECS/Systems/SpawnBedsPlugsSystem'.cs
@@ -0,0 +1,16 @@
+﻿using Entitas;
+
+namespace Code.ECS.Systems
+{
+	public sealed class SpawnBedsPlugsSystem : IInitializeSystem
+	{
+		private readonly Contexts _contexts;
+
+		public SpawnBedsPlugsSystem(Contexts contexts) => _contexts = contexts;
+
+		public void Initialize()
+		{
+			var path = "Environment/Trees Beds/Prefabs/Tree Bed Plug";
+		}
+	}
+}
