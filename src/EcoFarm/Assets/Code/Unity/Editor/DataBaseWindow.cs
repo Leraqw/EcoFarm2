aa3dfbac21290185a7d0baada269246c918c46ae
Rapuška <mikalajsiatkouski@gmail.com>
2022-10-25 19:36:54 +0300

    Created DataBaseWindow

---
+++ src/EcoFarm/Assets/Code/Unity/Editor/DataBaseWindow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Code.Unity.Editor
+{
+	public class DataBaseWindow : EditorWindow
+	{
+		[MenuItem("Tools/DataBase")]
+		private static void ShowWindow()
+		{
+			var window = GetWindow<DataBaseWindow>();
+			window.titleContent = new GUIContent("DataBase");
+			window.Show();
+		}
+
+		private void OnGUI()
+		{
+			if (GUILayout.Button("Create DataBase"))
+			{
+				Debug.Log("U clicked on Create DataBase");
+			}
+		}
+	}
+}
