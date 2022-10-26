dac49584787399f6557f0bcf68c11298dda64805
Rapuška <mikalajsiatkouski@gmail.com>
2022-10-26 19:19:50 +0300

    removed reducant code

--- src/EcoFarm/Assets/Code/Data/DataBase/DataBaseAccess.cs
+++ src/EcoFarm/Assets/Code/Data/DataBase/DataBaseAccess.cs
@@ -1,29 +1,23 @@
-﻿using Mono.Data.Sqlite;
-using UnityEngine;
+﻿using System;
+using Mono.Data.Sqlite;
 
 namespace Code.Data.DataBase
 {
 	public class DataBaseAccess
 	{
 		private const string DataBaseName = "URI=file:EcoFarm.db";
-		public int TreesCount => GetTreesCount();
 
-		public void DisplayData()
+		public int TreesCount => GetInt(Queries.SelectTreesQuantity);
+
+		public void DisplayData() => throw new NotImplementedException();
+
+		private static int GetInt(string query)
 		{
 			using var connection = new SqliteConnection(DataBaseName);
 			connection.Open();
 			using var command = connection.CreateCommand();
 
-			Debug.Log("Ur mama");
-		}
-
-		private int GetTreesCount()
-		{
-			using var connection = new SqliteConnection(DataBaseName);
-			connection.Open();
-			using var command = connection.CreateCommand();
-
-			command.CommandText = Queries.SelectTreesQuantity;
+			command.CommandText = query;
 			var reader = command.ExecuteReader();
 			reader.Read();
 			return reader.GetInt32(0);
--- src/EcoFarm/Assets/Code/Data/DataBase/Queries.cs
+++ src/EcoFarm/Assets/Code/Data/DataBase/Queries.cs
@@ -2,16 +2,6 @@ namespace Code.Data.DataBase
 {
 	public static class Queries
 	{
-		public static string CreateTableTree
-			=> "CREATE TABLE IF NOT EXISTS Tree "
-			   + "(TreeID INTEGER NOT NULL, "
-			   + "PRIMARY KEY(TreeID AUTOINCREMENT));";
-		
-		public static string CreateTableLevel
-			=> "CREATE TABLE IF NOT EXISTS Level "
-			   + "(LevelID INTEGER NOT NULL, "
-			   + "PRIMARY KEY(TreeID AUTOINCREMENT));";
-		
 		public static string SelectTreesQuantity
 			=> "SELECT Quantity FROM DevelopmentObjectsOnLevelStart;";
 	}
