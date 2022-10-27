8fc59410e89735fc36e3abb6fc5e7551fc1a1f6b
Rapuška <mikalajsiatkouski@gmail.com>
2022-10-27 16:28:48 +0300

    Refactoring

--- src/EcoFarm/Assets/Code/ECS/Systems/Product/Fruit/IsGrowthSystem.cs
+++ src/EcoFarm/Assets/Code/ECS/Systems/Product/Fruit/IsGrowthSystem.cs
@@ -1,4 +1,5 @@
-﻿using Code.Utils.Extensions.Entitas;
+﻿using Code.Utils.Extensions;
+using Code.Utils.Extensions.Entitas;
 using Entitas;
 
 namespace Code.ECS.Systems.Product.Fruit
@@ -7,19 +8,15 @@ namespace Code.ECS.Systems.Product.Fruit
 	{
 		private readonly IGroup<GameEntity> _entities;
 
-		public IsGrowthSystem(Contexts contexts)
-		{
-			_entities = contexts.game.GetGroup(GameMatcher.Growing);
-		}
+		public IsGrowthSystem(Contexts contexts) => _entities = contexts.game.GetGroup(GameMatcher.Growing);
 
 		public void Execute() => _entities.ForEach(Check);
 
-		private static void Check(GameEntity entity)
-		{
-			if (entity.growing.Value.IsContains(entity.GetLocalScale()) == false)
-			{
-				entity.RemoveGrowing();
-			}
-		}
+		private static void Check(GameEntity entity) => entity.Do(RemoveGrowing, @if: IsGrowth);
+
+		private static void RemoveGrowing(GameEntity entity) => entity.RemoveGrowing();
+
+		private static bool IsGrowth(GameEntity entity)
+			=> entity.growing.Value.IsContains(entity.GetLocalScale()) == false;
 	}
 }
--- src/EcoFarm/Assets/Code/Utils/Extensions/GenericExtensions.cs
+++ src/EcoFarm/Assets/Code/Utils/Extensions/GenericExtensions.cs
@@ -9,5 +9,15 @@ namespace Code.Utils.Extensions
 			action(@this);
 			return @this;
 		}
+
+		public static T Do<T>(this T @this, Action<T> action, Func<T, bool> @if)
+		{
+			if (@if.Invoke(@this))
+			{
+				action.Invoke(@this);	
+			}
+
+			return @this;
+		}
 	}
 }
