bb41446db186de74e64eccd94ac5b90d9c6ad20c
Rapuška <mikalajsiatkouski@gmail.com>
2022-10-27 18:51:38 +0300

    and more refactoring

--- src/EcoFarm/Assets/Code/ECS/Systems/Product/Fruit/SpawnFruitSystem.cs
+++ src/EcoFarm/Assets/Code/ECS/Systems/Product/Fruit/SpawnFruitSystem.cs
@@ -15,23 +15,20 @@ namespace Code.ECS.Systems.Product.Fruit
 			: base(contexts.game)
 			=> _contexts = contexts;
 
-		private GameEntity NewEntity => _contexts.game.CreateEntity();
-
 		protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
 			=> context.CreateCollector(GameMatcher.HasFruit);
 
-		protected override bool Filter(GameEntity entity)
-			=> true;
+		protected override bool Filter(GameEntity entity) => true;
 
 		protected override void Execute(List<GameEntity> entites) => entites.ForEach(SpawnFruitFor);
 
-		private void SpawnFruitFor(GameEntity tree) => Spawn(NewEntity, tree.spawnPosition);
+		private void SpawnFruitFor(GameEntity tree) => Spawn(tree.spawnPosition);
 
-		private static void Spawn(GameEntity fruit, Vector2 position)
-			=> fruit
-			   .Do((e) => e.AddSpawnPosition(position + Vector2.up)) // TODO: spawn on top of the tree
-			   .Do((e) => e.AddRequireView(ResourcePath.ApplePrefab))
-			   .Do((e) => e.AddGrowing(new Vector3Interval(Vector3.zero, Vector3.one)))
-			   .Do((e) => e.AddDebugName("Fruit"));
+		private void Spawn(Vector2 position)
+			=> _contexts.game.CreateEntity()
+			            .Do((e) => e.AddSpawnPosition(position + Vector2.up)) // TODO: spawn on top of the tree
+			            .Do((e) => e.AddRequireView(ResourcePath.ApplePrefab))
+			            .Do((e) => e.AddGrowing(new Vector3Interval(Vector3.zero, Vector3.one)))
+			            .Do((e) => e.AddDebugName("Fruit"));
 	}
 }
