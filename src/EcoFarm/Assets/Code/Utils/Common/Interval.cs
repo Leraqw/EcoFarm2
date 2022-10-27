500859618879d75be25248e3be7bfbe50a1d5aa8
Rapuška <mikalajsiatkouski@gmail.com>
2022-10-27 11:01:41 +0300

    Added Interval Type

---
+++ src/EcoFarm/Assets/Code/Utils/Common/Interval.cs
@@ -0,0 +1,14 @@
+﻿namespace Code.Utils.Common
+{
+	public class Interval<T>
+	{
+		public Interval(T startValue, T endValue)
+		{
+			StartValue = startValue;
+			EndValue = endValue;
+		}
+		
+		public T StartValue { get; }
+		public T EndValue { get; }
+	}
+}
