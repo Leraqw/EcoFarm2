35c757d4a3bc8e5ec6a589ea6e00868740a807bf
Rapuška <mikalajsiatkouski@gmail.com>
2022-10-28 22:12:42 +0300

    ProportionalScaleView

---
+++ src/EcoFarm/Assets/Code/Unity/ViewListeners/ProportionalScaleView.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Code.Unity.ViewListeners
+{
+	public class ProportionalScaleView : MonoBehaviour, IProportionalScaleListener
+	{
+		public void Register(GameEntity entity) => entity.AddProportionalScaleListener(this);
+
+		public void OnProportionalScale(GameEntity entity, float value) => transform.localScale = Vector3.one * value;
+	}
+}
