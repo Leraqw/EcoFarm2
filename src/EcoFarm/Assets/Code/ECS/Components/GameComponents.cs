71bfd9129f45768a965ae24e9e4219ef4ce3034b
Rapuška <mikalajsiatkouski@gmail.com>
2022-10-28 15:29:45 +0300

    Added position update on startup

--- src/EcoFarm/Assets/Code/ECS/Components/GameComponents.cs
+++ src/EcoFarm/Assets/Code/ECS/Components/GameComponents.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Code.Utils.ComponentsTemplates;
-using Entitas;
 using Entitas.CodeGeneration.Attributes;
 using UnityEngine;
 using static Entitas.CodeGeneration.Attributes.CleanupMode;
--- src/EcoFarm/Assets/Code/Unity/ViewListeners/PositionView.cs
+++ src/EcoFarm/Assets/Code/Unity/ViewListeners/PositionView.cs
@@ -1,10 +1,14 @@
-﻿using UnityEngine;
+﻿using Code.Utils.Extensions;
+using UnityEngine;
 
 namespace Code.Unity.ViewListeners
 {
 	public class PositionView : MonoBehaviour, IPositionListener
 	{
-		public void Register(GameEntity entity) => entity.AddPositionListener(this);
+		public void Register(GameEntity entity) => entity.Do((e) => e.AddPositionListener(this))
+		                                                 .Do(UpdatePosition, @if: (e) => e.hasPosition);
+
+		private void UpdatePosition(GameEntity entity) => OnPosition(entity, entity.position.Value);
 
 		public void OnPosition(GameEntity entity, Vector3 value) => transform.position = value;
 	}
