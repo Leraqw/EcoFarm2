a8e4c3481eb5a0059fae73c2a5171f50d95a58c0
Rapuška <mikalajsiatkouski@gmail.com>
2022-10-29 14:15:19 +0300

    Added MouseClickReceiver

---
+++ src/EcoFarm/Assets/Code/Unity/Receivers/MouseClickReceiver.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Code.Unity.Receivers
+{
+	public class MouseClickReceiver : MonoBehaviour
+	{
+		public void OnMouseDown()
+		{
+			Debug.Log("Mouse Clicked");
+		}
+	}
+}
