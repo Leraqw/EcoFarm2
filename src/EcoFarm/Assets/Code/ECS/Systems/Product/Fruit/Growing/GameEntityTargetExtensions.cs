a71acff9684303f682b74c3a0bc71bbe3698bae6
Rapuška <mikalajsiatkouski@gmail.com>
2022-10-30 17:14:01 +0300

    fixed error with not durated but reached target

--- src/EcoFarm/Assets/Code/ECS/Systems/Product/Fruit/Growing/GameEntityTargetExtensions.cs
+++ src/EcoFarm/Assets/Code/ECS/Systems/Product/Fruit/Growing/GameEntityTargetExtensions.cs
@@ -6,10 +6,12 @@ namespace Code.ECS.Systems.Product.Fruit.Growing
 	{
 		public static void OnTargetScaleReached(this GameEntity @this)
 			=> @this.Do((e) => e.RemoveTargetScale())
-			        .Do((e) => e.isGrowth = true);
+			        .Do((e) => e.isGrowth = true)
+			        .Do((e) => e.duration.Value = 0);
 
 		public static void OnTargetPositionReached(this GameEntity @this)
 			=> @this.Do((e) => e.RemoveTargetPosition())
-			        .Do((e) => e.isFell = true);
+			        .Do((e) => e.isFell = true)
+			        .Do((e) => e.duration.Value = 0);
 	}
 }
