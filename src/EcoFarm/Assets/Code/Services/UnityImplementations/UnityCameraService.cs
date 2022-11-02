afc2835a736a347c8816d91de4badcc6d60d9c9b
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-02 17:23:29 +0300

    Added CameraService

---
+++ src/EcoFarm/Assets/Code/Services/UnityImplementations/UnityCameraService.cs
@@ -0,0 +1,11 @@
+﻿using Code.Services.Interfaces;
+using UnityEngine;
+
+namespace Code.Services.UnityImplementations
+{
+	public class UnityCameraService : ICameraService
+	{
+		public Vector2 ScreenToWorldPoint(Vector2 screenPosition) 
+			=> Camera.main!.ScreenToWorldPoint(screenPosition);
+	}
+}
