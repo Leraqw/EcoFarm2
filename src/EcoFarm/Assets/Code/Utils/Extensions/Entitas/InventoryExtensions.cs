64b1d46a2c6bbd29f120a47eb03e334d39e0d21f
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-02 12:16:46 +0300

    Added SyncItemValueWithDebugSystem

--- src/EcoFarm/Assets/Code/Utils/Extensions/Entitas/InventoryExtensions.cs
+++ src/EcoFarm/Assets/Code/Utils/Extensions/Entitas/InventoryExtensions.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections.Generic;
+using Code.ECS.Components.ComplexComponentTypes;
 using static Code.Utils.StaticClasses.Constants.Temp;
 
 namespace Code.Utils.Extensions.Entitas
 {
 	public static class InventoryExtensions
 	{
+		public static void SetActualDebugName(this GameEntity @this) 
+			=> @this.ReplaceDebugName(@this.inventoryItem.Value);
+
+		private static void ReplaceDebugName(this GameEntity @this, Item item) 
+			=> @this.ReplaceDebugName($"Item: {item.Name} ({item.Count})");
+
 		public static void CreateInventoryItem(this GameContext @this, string name, int count)
 			=> @this.CreateEntity()
-			        .Do((e) => e.AddDebugName($"Item: {name} ({count})"))
-			        .Do((e) => e.AddFruitTypeId(AppleID))
 			        .Do((e) => e.AddInventoryItem((name, count)))
+			        .Do((e) => e.SetActualDebugName())
+			        .Do((e) => e.AddFruitTypeId(AppleID))
 			        .Do((e) => e.AddAttachedTo(@this.inventoryEntity.attachableIndex));
 		
 		public static IEnumerable<GameEntity> GetInventoryItems(this GameContext @this) 
