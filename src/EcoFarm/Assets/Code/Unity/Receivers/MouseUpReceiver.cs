7ca303ca32abd1706732671cd5716edc4cf6ab39
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-02 21:01:48 +0300

    Added MouseUpReceiver

---
+++ src/EcoFarm/Assets/Code/Unity/Receivers/MouseUpReceiver.cs
@@ -0,0 +1,12 @@
+﻿using Code.Unity.ViewListeners;
+using UnityEngine;
+
+namespace Code.Unity.Receivers
+{
+	public class MouseUpReceiver : MonoBehaviour
+	{
+		[SerializeField] private BaseViewListener _viewListener;
+
+		public void OnMouseUp() => _viewListener.Entity.isMouseUp = true;
+	}
+}
