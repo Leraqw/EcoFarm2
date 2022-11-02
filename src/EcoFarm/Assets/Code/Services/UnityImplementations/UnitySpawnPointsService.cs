02204ce9d3deb84cfe225e8953b1085f8f632e84
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-02 13:57:56 +0300

    Removing [FormerlySerializedAs]

--- src/EcoFarm/Assets/Code/Services/UnityImplementations/UnitySpawnPointsService.cs
+++ src/EcoFarm/Assets/Code/Services/UnityImplementations/UnitySpawnPointsService.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using Code.Services.Interfaces;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Code.Services.UnityImplementations
 {
 	[Serializable]
 	public class UnitySpawnPointsService : ISpawnPointsService
 	{
-		[FormerlySerializedAs("_treesSpawnPositions")] [SerializeField] private List<Transform> _trees;
-		[FormerlySerializedAs("_warehouseSpawnPosition")] [SerializeField] private Transform _warehouse;
-		[FormerlySerializedAs("_craneSpawnPosition")] [SerializeField] private Transform _crane;
+		[SerializeField] private List<Transform> _trees;
+		[SerializeField] private Transform _warehouse;
+		[SerializeField] private Transform _crane;
 
 		public IEnumerable<Vector2> Trees => _trees.Select((t) => (Vector2)t.position);
 		public Vector2 Warehouse => _warehouse.position;
--- src/EcoFarm/Assets/Code/Unity/UnityDependencies.cs
+++ src/EcoFarm/Assets/Code/Unity/UnityDependencies.cs
@@ -1,13 +1,12 @@
 ﻿using Code.Services.Interfaces;
 using Code.Services.UnityImplementations;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Code.Unity
 {
 	public class UnityDependencies : MonoBehaviour
 	{
-		[FormerlySerializedAs("_sceneObjects")] [SerializeField] private UnitySpawnPointsService _spawnPoints;
+		[SerializeField] private UnitySpawnPointsService _spawnPoints;
 
 		public ISpawnPointsService SpawnPoints => _spawnPoints;
 	}
