d3a6c321c370688c3006ead67b733738b842931e
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-03 16:34:00 +0300

    Added TreePostInitializationSystem

---
+++ src/EcoFarm/Assets/Code/ECS/Systems/Watering/Tree/TreePostInitializationSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Code.Utils.Extensions;
+using Entitas;
+
+namespace Code.ECS.Systems.Watering.Tree
+{
+	public sealed class TreePostInitializationSystem : ReactiveSystem<GameEntity>
+	{
+		public TreePostInitializationSystem(Contexts contexts)
+			: base(contexts.game) { }
+
+		protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
+			=> context.CreateCollector(GameMatcher.Tree);
+
+		protected override bool Filter(GameEntity entity) => entity.hasWatering == false;
+
+		protected override void Execute(List<GameEntity> entites) => entites.ForEach(PostInitialize);
+
+		private void PostInitialize(GameEntity tree) => tree.Do((e) => e.AddWatering(6)); // TODO: move to config
+	}
+}
