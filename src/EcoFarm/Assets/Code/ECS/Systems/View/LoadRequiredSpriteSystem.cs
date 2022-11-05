ae40b49948e565c89f0d2911abc6b23514f4cbb7
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-05 13:13:43 +0300

    Added LoadRequiredSpriteSystem

---
+++ src/EcoFarm/Assets/Code/ECS/Systems/View/LoadRequiredSpriteSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Code.Services.Interfaces;
+using Entitas;
+using static GameMatcher;
+
+namespace Code.ECS.Systems.View
+{
+	public sealed class LoadRequiredSpriteSystem : ReactiveSystem<GameEntity>
+	{
+		private readonly Contexts _contexts;
+
+		public LoadRequiredSpriteSystem(Contexts contexts)
+			: base(contexts.game)
+			=> _contexts = contexts;
+
+		private IResourcesService ResourcesService => _contexts.services.resourcesService.Value;
+
+		protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
+			=> context.CreateCollector(RequireSprite);
+
+		protected override bool Filter(GameEntity entity) => true;
+
+		protected override void Execute(List<GameEntity> entites) => entites.ForEach(Load);
+
+		private void Load(GameEntity entity) 
+			=> entity.AddSprite(ResourcesService.LoadSprite(entity.requireSprite.Value));
+	}
+}
