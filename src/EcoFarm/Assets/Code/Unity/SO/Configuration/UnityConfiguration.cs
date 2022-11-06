f2aaad9f8756a618b2fa16a164c5514f822b7e26
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-06 14:37:08 +0300

    Renaming

--- src/EcoFarm/Assets/Code/Unity/SO/Configuration/UnityConfiguration.cs
+++ src/EcoFarm/Assets/Code/Unity/SO/Configuration/UnityConfiguration.cs
@@ -1,13 +1,14 @@
 ﻿using Code.Services.Interfaces.Config;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Code.Unity.SO.Configuration
 {
 	[CreateAssetMenu(fileName = "Configuration", menuName = "ScriptableObject/Configuration")]
 	public class UnityConfiguration : ScriptableObject, IConfigurationService
 	{
-		[SerializeField] private BalanceConfig _balanceConfig;
+		[FormerlySerializedAs("_balanceConfig")] [SerializeField] private BalanceConfig _balance;
 
-		public IBalanceConfig Balance => _balanceConfig;
+		public IBalanceConfig Balance => _balance;
 	}
 }
