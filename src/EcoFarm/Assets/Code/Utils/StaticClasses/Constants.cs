4b3b5711e0f471a30c9f432c9418067655cef9de
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-06 17:54:07 +0300

    Removed Reducant Classes

--- src/EcoFarm/Assets/Code/Utils/StaticClasses/Constants.cs
+++ src/EcoFarm/Assets/Code/Utils/StaticClasses/Constants.cs
@@ -2,28 +2,6 @@
 {
 	public static class Constants
 	{
-		public static class ResourcePath
-		{
-			public static class Prefab
-			{
-				public const string Apple = "Products/Fruits/Prefabs/Apple";
-				public const string Tree = "Trees/Prefabs/Tree";
-				public const string BedPlug = "Environment/Trees Beds/Prefabs/Tree Bed Plug";
-				public const string Warehouse = "Environment/Warehouse/Prefabs/Warehouse";
-				public const string Crane = "Environment/Crane/Prefabs/Crane";
-				public const string Bucket = "Environment/Bucket/Prefabs/Bucket";
-			}
-
-			public static class Sprite
-			{
-				public static class Bucket
-				{
-					public const string Empty = "Environment/Bucket/Art/BucketEmpty";
-					public const string Filled = "Environment/Bucket/Art/BucketFilled";
-				}
-			}
-		}
-
 		public static class Temp
 		{
 			public const int AppleID = 1;
