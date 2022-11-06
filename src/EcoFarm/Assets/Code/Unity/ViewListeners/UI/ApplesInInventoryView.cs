2e4ca86f7db6332e10f35ab5da31abf983bcbcf4
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-06 19:42:22 +0300

    Added ApplesInInventoryView

---
+++ src/EcoFarm/Assets/Code/Unity/ViewListeners/UI/ApplesInInventoryView.cs
@@ -0,0 +1,19 @@
+﻿using Code.ECS.Components.ComplexComponentTypes;
+using TMPro;
+using UnityEngine;
+
+namespace Code.Unity.ViewListeners.UI
+{
+	public class ApplesInInventoryView : BaseViewListener, IInventoryItemListener
+	{
+		[SerializeField] private TextMeshProUGUI _text;
+
+		protected override void AddListener(GameEntity entity) => entity.AddInventoryItemListener(this);
+		protected override bool HasComponent(GameEntity entity) => entity.hasInventoryItem;
+
+		protected override void UpdateValue(GameEntity entity) => OnInventoryItem(entity, entity.inventoryItem);
+
+		public void OnInventoryItem(GameEntity entity, Item value) 
+			=> _text.text = entity.inventoryItem.Value.Count.ToString();
+	}
+}
