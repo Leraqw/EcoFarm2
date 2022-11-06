d802f758610fbdace774230f573cb8a1ea2e39e1
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-06 14:39:22 +0300

    Removed FormerlySerializedAs attribute

--- src/EcoFarm/Assets/Code/Unity/SO/Configuration/UnityConfiguration.cs
+++ src/EcoFarm/Assets/Code/Unity/SO/Configuration/UnityConfiguration.cs
@@ -1,13 +1,12 @@
 ﻿using Code.Services.Interfaces.Config;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Code.Unity.SO.Configuration
 {
 	[CreateAssetMenu(fileName = "Configuration", menuName = "ScriptableObject/Configuration")]
 	public class UnityConfiguration : ScriptableObject, IConfigurationService
 	{
-		[FormerlySerializedAs("_balanceConfig")] [SerializeField] private BalanceConfig _balance;
+		[SerializeField] private BalanceConfig _balance;
 
 		public IBalanceConfig Balance => _balance;
 	}
