7e7b7fefe3db473f90f7dd8212dfb0623712ab5e
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-09 01:15:38 +0300

    Added ViewListeners

---
+++ src/EcoFarm/Assets/Code/Unity/ViewListeners/SliderMaxValueView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.Unity.ViewListeners
+{
+	public class SliderMaxValueView : BaseViewListener, ISliderMaxValueListener
+	{
+		[SerializeField] private Slider _slider;
+
+		protected override void AddListener(GameEntity entity) => entity.AddSliderMaxValueListener(this);
+
+		protected override bool HasComponent(GameEntity entity) => entity.hasSliderMaxValue;
+
+		protected override void UpdateValue(GameEntity entity) => OnSliderMaxValue(entity, entity.sliderMaxValue);
+
+		public void OnSliderMaxValue(GameEntity entity, float value) => _slider.maxValue = value;
+	}
+}
---
+++ src/EcoFarm/Assets/Code/Unity/ViewListeners/SliderValueView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.Unity.ViewListeners
+{
+	public class SliderValueView : BaseViewListener, ISliderValueListener
+	{
+		[SerializeField] private Slider _slider;
+
+		protected override void AddListener(GameEntity entity) => entity.AddSliderValueListener(this);
+
+		protected override bool HasComponent(GameEntity entity) => entity.hasSliderValue;
+
+		protected override void UpdateValue(GameEntity entity) => OnSliderValue(entity, entity.sliderValue);
+
+		public void OnSliderValue(GameEntity entity, float value) => _slider.value = value;
+	}
+}
