8fcce09fc09dbb99495348020b3b8c902ec5f09e
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-08 19:58:57 +0300

    Added ButtonCLickReveiver

---
+++ src/EcoFarm/Assets/Code/Unity/Receivers/ButtonClickReceiver.cs
@@ -0,0 +1,18 @@
+﻿using Code.Unity.ViewListeners;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.Unity.Receivers
+{
+	public class ButtonClickReceiver : MonoBehaviour
+	{
+		[SerializeField] private Button _button;
+		[SerializeField] private BaseViewListener _viewListener;
+
+		private void OnEnable() => _button.onClick.AddListener(OnButtonClick);
+		
+		private void OnDisable() => _button.onClick.RemoveListener(OnButtonClick);
+
+		private void OnButtonClick() => _viewListener.Entity.isButtonClick = true;
+	}
+}
