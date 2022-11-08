0ed6fbfbd25d60077e59147aefb0f82146fb18a1
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-08 21:22:57 +0300

    Added ClosableWindow class

---
+++ src/EcoFarm/Assets/Code/Unity/Containers/ClosableWindow.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.Unity.Containers
+{
+	public class ClosableWindow : MonoBehaviour
+	{
+		[field: SerializeField] public Button ButtonClose { get; private set; }
+	}
+}
