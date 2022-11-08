cdbd21c745e9e2de5dc998b197ffdd649c4dd57e
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-08 19:27:10 +0300

    Unity Meta-files

---
+++ src/EcoFarm/Assets/Code/Unity/ViewListeners/EnabledView.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Code.Unity.ViewListeners
+{
+	public class EnabledView : BaseViewListener, IEnabledListener
+	{
+		protected override void AddListener(GameEntity entity) => entity.AddEnabledListener(this);
+
+		protected override bool HasComponent(GameEntity entity) => true;
+
+		protected override void UpdateValue(GameEntity entity) => OnEnabled(entity);
+
+		public void OnEnabled(GameEntity entity) => enabled = entity.isEnabled;
+	}
+}
