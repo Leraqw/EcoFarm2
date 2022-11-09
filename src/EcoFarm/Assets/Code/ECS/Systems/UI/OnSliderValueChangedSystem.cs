6d29b34bea820076efc4e0bbc5e0eae910d2b28f
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-09 14:55:53 +0300

    Cleanup

--- src/EcoFarm/Assets/Code/ECS/Systems/UI/OnSliderValueChangedSystem.cs
+++ src/EcoFarm/Assets/Code/ECS/Systems/UI/OnSliderValueChangedSystem.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using Code.Utils.Extensions;
 using Code.Utils.Extensions.Entitas;
 using Entitas;
 using static System.Globalization.CultureInfo;
