916373e332d8901cead6f047e67682dedd47ce96
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-09 14:23:06 +0300

    Added CurrentValue property to WindowSell

--- src/EcoFarm/Assets/Code/Unity/Containers/WindowSell.cs
+++ src/EcoFarm/Assets/Code/Unity/Containers/WindowSell.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Code.Unity.Containers
@@ -6,5 +7,6 @@ namespace Code.Unity.Containers
 	public class WindowSell : MonoBehaviour
 	{
 		[field: SerializeField] public Slider CountToSellSlider { get; private set; }
+		[field: SerializeField] public TextMeshProUGUI CurrentValue { get; private set; }
 	}
 }
