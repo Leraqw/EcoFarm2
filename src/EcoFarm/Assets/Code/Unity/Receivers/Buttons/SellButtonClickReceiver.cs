0897be2e68683b1a488b52bcd69587becaad12f6
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-09 16:38:37 +0300

    Added SellButtonClickReceiver

---
+++ src/EcoFarm/Assets/Code/Unity/Receivers/Buttons/SellButtonClickReceiver.cs
@@ -0,0 +1,12 @@
+﻿using Code.Unity.Valuables;
+using UnityEngine;
+
+namespace Code.Unity.Receivers.Buttons
+{
+	public class SellButtonClickReceiver : BaseButtonClickReceiver
+	{
+		[SerializeField] private Valuable _valuable;
+
+		protected override void OnButtonClick() => Context.sellDealEntity.ReplaceCount(_valuable.Value);
+	}
+}
---
+++ src/EcoFarm/Assets/Code/Unity/Valuables/Valuable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Code.Unity.Valuables
+{
+	public abstract class Valuable : MonoBehaviour
+	{
+		public abstract int Value { get; }
+	}
+}
