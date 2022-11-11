5d16c4cd8185c8a971a210c37571e311cffd3cf6
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-11 17:30:34 +0300

    Added Goal

--- src/EcoFarm/Assets/Code/Data/Config/Editor/TempDataCreator.cs
+++ src/EcoFarm/Assets/Code/Data/Config/Editor/TempDataCreator.cs
@@ -3,33 +3,51 @@ using EcoFarmDataModule;
 using Newtonsoft.Json;
 using UnityEngine;
 using static Code.Utils.StaticClasses.Constants;
+using static Newtonsoft.Json.PreserveReferencesHandling;
 using Tree = EcoFarmDataModule.Tree;
 
 namespace Code.Data.Config.Editor
 {
 	public static class TempDataCreator
 	{
+		private static JsonSerializerSettings WithReferences => new() { PreserveReferencesHandling = Objects };
+
 		public static void Create() => Serialization(NewStorage());
 
 		private static Storage NewStorage()
-			=> new()
+		{
+			var apple = new Product
 			{
-				Levels = new[] { new Level { Order = 1, TreesCount = 5 } },
+				Title = "Apple",
+				Description = "Is a sweet, edible fruit produced by an apple tree.",
+				Price = 2,
+			};
+
+			return new Storage
+			{
+				Levels = new[]
+				{
+					new Level
+					{
+						Order = 1,
+						TreesCount = 5,
+						Goals = new Goal[]
+						{
+							new GoalByDevelopmentObject { DevelopmentObject = apple, TargetQuantity = 5 }
+						}
+					}
+				},
 				Trees = new[]
 				{
 					new Tree
 					{
 						Title = "Apple Tree",
 						Description = "Giving apples",
-						Product = new Product
-						{
-							Title = "Apple",
-							Description = "Is a sweet, edible fruit produced by an apple tree.",
-							Price = 2,
-						}
+						Product = apple
 					}
 				}
 			};
+		}
 
 		private static void Serialization(Storage storage)
 		{
@@ -38,6 +56,6 @@ namespace Code.Data.Config.Editor
 		}
 
 		private static string SerializeObject(Storage storage)
-			=> JsonConvert.SerializeObject(storage, Formatting.Indented);
+			=> JsonConvert.SerializeObject(storage, Formatting.Indented, WithReferences);
 	}
 }
--- src/EcoFarm/Assets/Code/Data/StorageJson/StorageAccess.cs
+++ src/EcoFarm/Assets/Code/Data/StorageJson/StorageAccess.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Code.Utils.StaticClasses;
 using EcoFarmDataModule;
 using Newtonsoft.Json;
+using static Newtonsoft.Json.PreserveReferencesHandling;
 
 namespace Code.Data.StorageJson
 {
@@ -12,11 +13,13 @@ namespace Code.Data.StorageJson
 
 		public StorageAccess() => LoadStorage();
 
+		private static JsonSerializerSettings WithReferences => new() { PreserveReferencesHandling = Objects };
+
 		public int TreesCount => _storage.Levels.First().TreesCount;
 
 		public Tree AppleTree => _storage.Trees.First();
 
-		private void LoadStorage() => _storage = JsonConvert.DeserializeObject<Storage>(GetJson());
+		private void LoadStorage() => _storage = JsonConvert.DeserializeObject<Storage>(GetJson(), WithReferences);
 
 		private static string GetJson() => File.ReadAllText(Constants.PathToStorage);
 	}
