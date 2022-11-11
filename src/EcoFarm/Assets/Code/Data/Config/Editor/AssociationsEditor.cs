6bdbb262a513ea40c1ebb8bf580f025282c35c56
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-11 21:18:59 +0300

    Added Associations class

---
+++ src/EcoFarm/Assets/Code/Data/Config/Editor/AssociationsEditor.cs
@@ -0,0 +1,18 @@
+﻿// ReSharper disable Unity.PerformanceCriticalCodeInvocation - we don't care about performance in the editor
+using Code.Data.ToUnity;
+using UnityEditor;
+using UnityEngine;
+
+namespace Code.Data.Config.Editor
+{
+	[CustomEditor(typeof(Associations))]
+	public class AssociationsEditor : UnityEditor.Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+
+			GUILayout.Button("Update Resources").OnPress(((Associations)target).UpdateResources);
+		}
+	}
+}
