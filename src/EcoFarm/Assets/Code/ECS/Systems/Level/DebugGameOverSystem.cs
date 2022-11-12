75d6fe4f2e45c84bddc03adbc9c79d64451a780d
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-12 18:00:39 +0300

    Added DebugGameOverSystem

---
+++ src/EcoFarm/Assets/Code/ECS/Systems/Level/DebugGameOverSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Entitas;
+using UnityEngine;
+using static GameMatcher;
+
+namespace Code.ECS.Systems.Level
+{
+	public sealed class DebugGameOverSystem : ReactiveSystem<GameEntity>
+	{
+		public DebugGameOverSystem(Contexts contexts)
+			: base(contexts.game) { }
+
+		protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
+			=> context.CreateCollector(AllOf(DurationUp, LevelTimer));
+
+		protected override bool Filter(GameEntity entity) => true;
+
+		protected override void Execute(List<GameEntity> entites)
+		{
+			foreach (var _ in entites)
+			{
+				Debug.Log("Game Over");
+			}
+		}
+	}
+}
