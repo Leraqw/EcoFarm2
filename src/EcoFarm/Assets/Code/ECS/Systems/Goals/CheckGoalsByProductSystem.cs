0ba7a2369a0feb4c68887e7785cee1f7cc9ed1e1
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-12 15:29:50 +0300

    CheckGoalsByProductSystem sepparated on 2

--- src/EcoFarm/Assets/Code/ECS/Systems/Goals/CheckGoalsByProductSystem.cs
+++ src/EcoFarm/Assets/Code/ECS/Systems/Goals/CheckGoalsByProductSystem.cs
@@ -1,35 +1,21 @@
 ﻿using System.Collections.Generic;
 using Code.Utils.Extensions;
 using Entitas;
-using Code.Utils.Extensions.Entitas;
 using static GameMatcher;
 
 namespace Code.ECS.Systems.Goals
 {
 	public sealed class CheckGoalsByProductSystem : ReactiveSystem<GameEntity>
 	{
-		private readonly IGroup<GameEntity> _goals;
-
 		public CheckGoalsByProductSystem(Contexts contexts)
-			: base(contexts.game)
-			=> _goals = contexts.game.GetGroup(AllOf(Goal, GameMatcher.Product).NoneOf(GoalCompleted));
+			: base(contexts.game) { }
 
 		protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
-			=> context.CreateCollector(InventoryItem);
+			=> context.CreateCollector(CurrentQuantity);
 
 		protected override bool Filter(GameEntity entity) => true;
 
-		protected override void Execute(List<GameEntity> entites) => entites.ForEach(Check);
-
-		private void Check(GameEntity item) => _goals
-		                                       .Do((g) => UpdateQuantities(g, item))
-		                                       .Do(MarkCompleted);
-
-		private void UpdateQuantities(IGroup<GameEntity> goals, GameEntity item)
-			=> goals.ForEach((g) => g.ReplaceCurrentQuantity(item.inventoryItem.Value.Count));
-
-		private void MarkCompleted(IGroup<GameEntity> goals)
-			=> goals.ForEach(Mark, @if: IsCompleted);
+		protected override void Execute(List<GameEntity> entites) => entites.ForEach(Mark, @if: IsCompleted);
 
 		private static bool IsCompleted(GameEntity goal)
 			=> goal.currentQuantity.Value >= goal.goal.Value.TargetQuantity;
---
+++ src/EcoFarm/Assets/Code/ECS/Systems/Goals/UpdateGoalProgressSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Entitas;
+using Code.Utils.Extensions.Entitas;
+using static GameMatcher;
+
+namespace Code.ECS.Systems.Goals
+{
+	public sealed class UpdateGoalProgressSystem : ReactiveSystem<GameEntity>
+	{
+		private readonly IGroup<GameEntity> _goals;
+
+		public UpdateGoalProgressSystem(Contexts contexts)
+			: base(contexts.game)
+			=> _goals = contexts.game.GetGroup(AllOf(Goal, GameMatcher.Product).NoneOf(GoalCompleted));
+
+		protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
+			=> context.CreateCollector(InventoryItem);
+
+		protected override bool Filter(GameEntity entity) => true;
+
+		protected override void Execute(List<GameEntity> items) => items.ForEach(UpdateGoals);
+
+		private void UpdateGoals(GameEntity item) => _goals.ForEach((goal) => UpdateProgress(goal, item));
+
+		private static void UpdateProgress(GameEntity entity, GameEntity item)
+			=> entity.ReplaceCurrentQuantity(item.inventoryItem.Value.Count);
+	}
+}
