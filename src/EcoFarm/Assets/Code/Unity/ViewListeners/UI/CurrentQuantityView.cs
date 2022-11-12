39d79902fd3de21983338c4fd8f63cf4e62aad5c
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-12 15:41:53 +0300

    Added CurrentQuantityView

---
+++ src/EcoFarm/Assets/Code/Unity/ViewListeners/UI/CurrentQuantityView.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Code.Unity.ViewListeners.UI
+{
+	public class CurrentQuantityView : BaseViewListener, ICurrentQuantityListener
+	{
+		[SerializeField] private TextMeshProUGUI _textMesh;
+
+		protected override void AddListener(GameEntity entity) => entity.AddCurrentQuantityListener(this);
+
+		protected override bool HasComponent(GameEntity entity) => entity.hasCurrentQuantity;
+
+		protected override void UpdateValue(GameEntity entity) => OnCurrentQuantity(entity, entity.currentQuantity);
+
+		public void OnCurrentQuantity(GameEntity entity, int value) 
+			=> _textMesh.text = $"{value} / {entity.goal.Value.TargetQuantity}";
+	}
+}
