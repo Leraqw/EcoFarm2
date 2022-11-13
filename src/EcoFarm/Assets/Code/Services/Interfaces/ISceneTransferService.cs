f92e584aed52d1aa8b48c8e170af1e573bacefa6
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-13 20:46:27 +0300

    +GameResultScene

---
+++ src/EcoFarm/Assets/Code/Services/Interfaces/ISceneTransferService.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace Code.Services.Interfaces
+{
+	public interface ISceneTransferService : IService
+	{
+		void ToMainMenuScene();
+		void ToGameplayScene();
+		void ToGameResultScene();
+	}
+
+	public class UnitySceneTransferService : ISceneTransferService
+	{
+		public void ToMainMenuScene()
+		{
+			SceneManager.LoadScene("MainMenuScene");
+		}
+
+		public void ToGameplayScene()
+		{
+			SceneManager.LoadScene("GameplayScene");
+		}
+
+		public void ToGameResultScene()
+		{
+			SceneManager.LoadScene("GameResultScene");
+		}
+	}
+}
