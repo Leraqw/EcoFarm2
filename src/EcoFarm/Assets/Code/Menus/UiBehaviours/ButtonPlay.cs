cc92e0631839517d7c4f2cc8ce4941d78d960393
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-13 15:19:17 +0300

    Added ButtonPlay

---
+++ src/EcoFarm/Assets/Code/Menus/UiBehaviours/ButtonPlay.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Code.Menus.UiBehaviours
+{
+	public class ButtonPlay : MonoBehaviour
+	{
+		[SerializeField] private Button _button;
+
+		private void OnEnable() => _button.onClick.AddListener(ToGameplayScene);
+
+		private void OnDisable() => _button.onClick.RemoveListener(ToGameplayScene);
+
+		private void ToGameplayScene() => SceneManager.LoadScene("GameplayScene");
+	}
+}
