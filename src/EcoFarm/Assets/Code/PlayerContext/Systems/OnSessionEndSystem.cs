d0562b9542fd209f85f3832699a786218e57017a
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-13 21:00:46 +0300

    On Session Endee — is Loading scene

--- src/EcoFarm/Assets/Code/PlayerContext/Systems/OnSessionEndSystem.cs
+++ src/EcoFarm/Assets/Code/PlayerContext/Systems/OnSessionEndSystem.cs
@@ -1,23 +1,27 @@
 ﻿using System.Collections.Generic;
+using Code.Services.Interfaces;
 using Entitas;
-using UnityEngine;
 using static Code.PlayerContext.CustomTypes.SessionResult;
 
 namespace Code.PlayerContext.Systems
 {
 	public sealed class OnSessionEndSystem : ReactiveSystem<PlayerEntity>
 	{
+		private readonly Contexts _contexts;
+
 		public OnSessionEndSystem(Contexts contexts)
-			: base(contexts.player) { }
+			: base(contexts.player)
+			=> _contexts = contexts;
 
 		protected override ICollector<PlayerEntity> GetTrigger(IContext<PlayerEntity> context)
 			=> context.CreateCollector(PlayerMatcher.SessionResult);
+
+		private ISceneTransferService SceneTransfer => _contexts.services.sceneTransferService.Value;
 
 		protected override bool Filter(PlayerEntity entity) => entity.sessionResult != None;
 
 		protected override void Execute(List<PlayerEntity> entites) => entites.ForEach(OnEnd);
 
-		private static void OnEnd(PlayerEntity entity) 
-			=> Debug.Log($"At this moment, the session has ended with the result: {entity.sessionResult.Value}");
+		private void OnEnd(PlayerEntity entity) => SceneTransfer.ToGameResultScene();
 	}
 }
