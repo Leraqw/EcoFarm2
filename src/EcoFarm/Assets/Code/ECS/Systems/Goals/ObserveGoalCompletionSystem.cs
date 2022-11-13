2010d71d7b479cdcf172399b9a43632f19f14c93
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-13 20:01:51 +0300

    +ObserveGoalCompletionSystem

---
+++ src/EcoFarm/Assets/Code/ECS/Systems/Goals/ObserveGoalCompletionSystem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Entitas;
+using static Code.PlayerContext.CustomTypes.SessionResult;
+using static GameMatcher;
+
+namespace Code.ECS.Systems.Goals
+{
+	public sealed class ObserveGoalCompletionSystem : ReactiveSystem<GameEntity>
+	{
+		private readonly Contexts _contexts;
+		private readonly IGroup<GameEntity> _notCompletedGoals;
+
+		public ObserveGoalCompletionSystem(Contexts contexts)
+			: base(contexts.game)
+		{
+			_notCompletedGoals = contexts.game.GetGroup(AllOf(Goal).NoneOf(GoalCompleted));
+			_contexts = contexts;
+		}
+
+		protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
+			=> context.CreateCollector(GoalCompleted);
+
+		protected override bool Filter(GameEntity entity) => true;
+
+		protected override void Execute(List<GameEntity> entites)
+		{
+			if (AllGoalsIsCompleted)
+			{
+				_contexts.player.playerEntity.ReplaceSessionResult(Victory);
+			}
+		}
+
+		private bool AllGoalsIsCompleted => _notCompletedGoals.GetEntities().Any() == false;
+	}
+}
