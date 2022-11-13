c34047e8da5bc92e1b952fd70f616192d2cba8c0
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-13 20:30:44 +0300

    Moved interfaces

--- src/EcoFarm/Assets/Code/Services/Game/Interfaces/IGameServices.cs
+++ src/EcoFarm/Assets/Code/Services/Game/Interfaces/IGameServices.cs
@@ -3,5 +3,8 @@ using Code.Services.Interfaces.Config;
 
 namespace Code.Services.Game.Interfaces
 {
-	public interface IGameServices : ISpawnPointsService, IConfigurationService, IUiService { }
+	public interface IGameServices
+		: ISpawnPointsService,
+		  IConfigurationService,
+		  IUiService { }
 }
