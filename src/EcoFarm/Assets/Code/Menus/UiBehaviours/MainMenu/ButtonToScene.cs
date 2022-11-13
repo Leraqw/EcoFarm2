327f3778e7017022a7ec737f7169944578946a57
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-13 20:55:48 +0300

    Scene Load Logic Incapsulated

--- src/EcoFarm/Assets/Code/Menus/UiBehaviours/MainMenu/ButtonToScene.cs
+++ src/EcoFarm/Assets/Code/Menus/UiBehaviours/MainMenu/ButtonToScene.cs
@@ -1,6 +1,6 @@
-﻿using Code.Unity.CustomTypes;
+﻿using Code.Services.Interfaces;
+using Code.Unity.CustomTypes;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace Code.Menus.UiBehaviours.MainMenu
@@ -10,10 +10,13 @@ namespace Code.Menus.UiBehaviours.MainMenu
 		[SerializeField] private Button _button;
 		[SerializeField] private SceneField _scene;
 
+		private static ISceneTransferService SceneTransfer
+			=> Contexts.sharedInstance.services.sceneTransferService.Value;
+
 		private void OnEnable() => _button.onClick.AddListener(ToGameplayScene);
 
 		private void OnDisable() => _button.onClick.RemoveListener(ToGameplayScene);
 
-		private void ToGameplayScene() => SceneManager.LoadScene(_scene);
+		private void ToGameplayScene() => SceneTransfer.ToScene(_scene);
 	}
 }
