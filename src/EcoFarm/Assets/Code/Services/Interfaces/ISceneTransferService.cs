cad399b140363df7b7760c2394032a83813b38de
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-13 20:47:27 +0300

    +unity SceneTransferService realization

--- src/EcoFarm/Assets/Code/Services/Interfaces/ISceneTransferService.cs
+++ src/EcoFarm/Assets/Code/Services/Interfaces/ISceneTransferService.cs
@@ -1,29 +1,9 @@
-﻿using UnityEngine.SceneManagement;
-
-namespace Code.Services.Interfaces
+﻿namespace Code.Services.Interfaces
 {
 	public interface ISceneTransferService : IService
 	{
 		void ToMainMenuScene();
 		void ToGameplayScene();
 		void ToGameResultScene();
-	}
-
-	public class UnitySceneTransferService : ISceneTransferService
-	{
-		public void ToMainMenuScene()
-		{
-			SceneManager.LoadScene("MainMenuScene");
-		}
-
-		public void ToGameplayScene()
-		{
-			SceneManager.LoadScene("GameplayScene");
-		}
-
-		public void ToGameResultScene()
-		{
-			SceneManager.LoadScene("GameResultScene");
-		}
 	}
 }
---
+++ src/EcoFarm/Assets/Code/Services/UnityImplementations/UnitySceneTransferService.cs
@@ -0,0 +1,14 @@
+﻿using Code.Services.Interfaces;
+using UnityEngine.SceneManagement;
+
+namespace Code.Services.UnityImplementations
+{
+	public class UnitySceneTransferService : ISceneTransferService
+	{
+		public void ToMainMenuScene() => SceneManager.LoadScene("MainMenuScene");
+
+		public void ToGameplayScene() => SceneManager.LoadScene("GameplayScene");
+
+		public void ToGameResultScene() => SceneManager.LoadScene("GameResultScene");
+	}
+}
