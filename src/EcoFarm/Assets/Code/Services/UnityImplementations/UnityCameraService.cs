13179f21e8fc5948e5ab81d2ca10591154692ed1
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-14 14:56:49 +0300

    Fixed Camera NullRef

--- src/EcoFarm/Assets/Code/Services/UnityImplementations/UnityCameraService.cs
+++ src/EcoFarm/Assets/Code/Services/UnityImplementations/UnityCameraService.cs
@@ -5,11 +5,9 @@ namespace Code.Services.UnityImplementations
 {
 	public class UnityCameraService : ICameraService
 	{
-		private readonly Camera _camera;
+		private static Camera Camera => Camera.main;
 
-		public UnityCameraService() => _camera = Camera.main;
-
-		public Vector2 ScreenToWorldPoint(Vector2 screenPosition) 
-			=> _camera.ScreenToWorldPoint(screenPosition);
+		public Vector2 ScreenToWorldPoint(Vector2 screenPosition)
+			=> Camera.ScreenToWorldPoint(screenPosition);
 	}
 }
