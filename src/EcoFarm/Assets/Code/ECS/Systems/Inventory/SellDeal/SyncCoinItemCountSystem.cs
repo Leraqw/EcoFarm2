a54c531022f97faeb9b3ae82b650c8175700ca3f
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-14 20:58:28 +0300

    +SyncCoinItemCountSystem

---
+++ src/EcoFarm/Assets/Code/ECS/Systems/Inventory/SellDeal/SyncCoinItemCountSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Entitas;
+
+namespace Code.ECS.Systems.Inventory.SellDeal
+{
+	public sealed class SyncCoinItemCountSystem : ReactiveSystem<ContextEntity>
+	{
+		public SyncCoinItemCountSystem(Contexts contexts)
+			: base(contexts.context) { }
+
+		protected override ICollector<ContextEntity> GetTrigger(IContext<ContextEntity> context)
+			=> context.CreateCollector(ContextMatcher.Matcher);
+
+		protected override bool Filter(ContextEntity entity)
+			=> true;
+
+		protected override void Execute(List<ContextEntity> entites)
+		{
+			foreach (ContextEntity e in entites)
+			{
+				
+			}
+		}
+	}
+}
