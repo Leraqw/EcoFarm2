fcfb64ce347410ae2601338f6abfeb306c1523be
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-16 17:19:55 +0300

    Reserialized

--- src/EcoFarm/Assets/Code/Data/ToUnity/SpriteSheet.cs
+++ src/EcoFarm/Assets/Code/Data/ToUnity/SpriteSheet.cs
@@ -4,14 +4,13 @@ using Code.Data.StorageJson;
 using EcoFarmDataModule;
 using UnityEngine;
 using Code.Utils.Extensions;
-using UnityEngine.Serialization;
 
 namespace Code.Data.ToUnity
 {
 	[CreateAssetMenu(fileName = "SpriteSheet", menuName = "ScriptableObject/SpriteSheet")]
 	public class SpriteSheet : ScriptableObject
 	{
-		[FormerlySerializedAs("_associations")] [SerializeField] private List<Association> _products;
+		[SerializeField] private List<Association> _products;
 
 		public Dictionary<string, Sprite> Dictionary => _products.ToDictionary((x) => x.Title, (x) => x.Sprite);
 
