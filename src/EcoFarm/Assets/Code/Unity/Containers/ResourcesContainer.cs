3453f8c3654018632d2b2c339ac28ab0b0969bc2
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-18 01:43:04 +0300

    +ResourcesContainer

---
+++ src/EcoFarm/Assets/Code/Unity/Containers/ResourcesContainer.cs
@@ -0,0 +1,11 @@
+﻿using Code.Unity.ViewListeners.UI;
+using UnityEngine;
+
+namespace Code.Unity.Containers
+{
+	public class ResourcesContainer : MonoBehaviour
+	{
+		[field: SerializeField] public ProgressBarView WaterIndicator  { get; private set; }
+		[field: SerializeField] public ProgressBarView EnergyIndicator { get; private set; }
+	}
+}
