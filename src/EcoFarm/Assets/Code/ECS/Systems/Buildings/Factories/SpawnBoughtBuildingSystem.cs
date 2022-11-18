9ab029d6bf73715fa5b3a03a536750bd560c0a62
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-19 02:06:18 +0300

    Adding Consumption For Factory

--- src/EcoFarm/Assets/Code/ECS/Systems/Buildings/Factories/SpawnBoughtBuildingSystem.cs
+++ src/EcoFarm/Assets/Code/ECS/Systems/Buildings/Factories/SpawnBoughtBuildingSystem.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Code.Services.Game.Interfaces.Config.ResourcesConfigs;
 using Code.Utils.Extensions;
 using Code.Utils.Extensions.Entitas;
+using EcoFarmDataModule;
 using Entitas;
 using static GameMatcher;
 
@@ -19,8 +21,11 @@ namespace Code.ECS.Systems.Buildings.Factories
 			_signs = contexts.game.GetGroup(Sign);
 		}
 
+		// TODO: Remove Linq
+		private FactoryBuilding FirstFactory => ((FactoryBuilding)_contexts.game.storage.Value.Buildings.First());
+
 		protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
-			=> context.CreateCollector(AllOf(UiElement, Bought, Building));
+			=> context.CreateCollector(AllOf(UiElement, Bought, GameMatcher.Building));
 
 		protected override bool Filter(GameEntity entity) => true;
 
@@ -34,7 +39,9 @@ namespace Code.ECS.Systems.Buildings.Factories
 		private void Replace(GameEntity sign)
 			=> sign.Do((e) => e.RemoveView())
 			       .Do((e) => e.ReplaceViewPrefab(Resource.Prefab.Factory))
-			       .Do((e) => e.isOccupied = true);
+			       .Do((e) => e.isOccupied = true)
+			       .Do((e) => e.AddConsumer(_contexts.game.energyResourceEntity.consumable))
+			       .Do((e) => e.AddConsumptionCoefficient(FirstFactory.ResourceConsumptionCoefficient));
 
 		private IResourceConfig Resource => _contexts.services.configurationService.Value.Resource;
 	}
