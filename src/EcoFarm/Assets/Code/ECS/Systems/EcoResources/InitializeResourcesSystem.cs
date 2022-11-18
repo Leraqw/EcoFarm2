e1af6cb35ae9b0bf802c35b5a72bde600051f012
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-18 22:37:43 +0300

    Using Config in initialization

--- src/EcoFarm/Assets/Code/ECS/Systems/EcoResources/InitializeResourcesSystem.cs
+++ src/EcoFarm/Assets/Code/ECS/Systems/EcoResources/InitializeResourcesSystem.cs
@@ -1,7 +1,9 @@
 ﻿using Code.ECS.Components.ComplexComponentTypes;
+using Code.Services.Game.Interfaces.Config;
 using Code.Unity.Containers;
 using Code.Utils.Extensions;
 using Entitas;
+using TMPro;
 
 namespace Code.ECS.Systems.EcoResources
 {
@@ -13,18 +15,28 @@ namespace Code.ECS.Systems.EcoResources
 
 		private WindowResources WindowsResources => _contexts.services.uiService.Value.Windows.Resources;
 
+		private IBalanceConfig Balance => _contexts.services.configurationService.Value.Balance;
+
+		private int EnergyStart => Balance.Energy.StartValue;
+
+		private int EnergyMax => Balance.Energy.MaxValue;
+
+		private int WaterStart => Balance.Water.StartValue;
+
+		private int WaterMax => Balance.Water.MaxValue;
+
 		public void Initialize()
 		{
 			_contexts.game.CreateEntity()
 			         .Do((e) => e.AddDebugName("Resource - Energy"))
 			         .Do((e) => e.isEnergyResource = true)
-			         .Do((e) => e.AddProgressBar(new ProgressBarValues()))
+			         .Do((e) => e.AddProgressBar(new ProgressBarValues { Max = EnergyMax, Current = EnergyStart }))
 			         .Do((e) => e.AddView(WindowsResources.EnergyIndicator.gameObject));
 
 			_contexts.game.CreateEntity()
 			         .Do((e) => e.AddDebugName("Resource - Water"))
 			         .Do((e) => e.isWaterResource = true)
-			         .Do((e) => e.AddProgressBar(new ProgressBarValues()))
+			         .Do((e) => e.AddProgressBar(new ProgressBarValues { Max = WaterMax, Current = WaterStart }))
 			         .Do((e) => e.AddView(WindowsResources.WaterIndicator.gameObject));
 		}
 	}
