ee2e39817d4a0b84a972903cc7caf2a24e49367a
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-19 01:53:56 +0300

    Spent and Check Money For Resource Renew

--- src/EcoFarm/Assets/Code/ECS/Systems/EcoResources/OnResourceButtonClickSystem.cs
+++ src/EcoFarm/Assets/Code/ECS/Systems/EcoResources/OnResourceButtonClickSystem.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using Code.ECS.Components.ComplexComponentTypes;
+using Code.Utils.Extensions;
 using Code.Utils.Extensions.Entitas;
 using Entitas;
 using static GameMatcher;
@@ -7,15 +9,22 @@ namespace Code.ECS.Systems.EcoResources
 {
 	public sealed class OnResourceButtonClickSystem : ReactiveSystem<GameEntity>
 	{
-		public OnResourceButtonClickSystem(Contexts contexts) : base(contexts.game) { }
+		private readonly Contexts _contexts;
+		public OnResourceButtonClickSystem(Contexts contexts) : base(contexts.game) => _contexts = contexts;
+
+		private Item Coins => _contexts.game.coinEntity.inventoryItem.Value;
 
 		protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
 			=> context.CreateCollector(AllOf(ProgressBar, MouseDown));
 
 		protected override bool Filter(GameEntity entity) => true;
 
-		protected override void Execute(List<GameEntity> entites) => entites.ForEach(Handle);
+		protected override void Execute(List<GameEntity> entites) => entites.ForEach(Renew, @if: EnoughMoney);
 
-		private void Handle(GameEntity resource) => resource.UpdateResourceCurrentValue(resource.progressBar.Value.Max);
+		private bool EnoughMoney(GameEntity resource) => Coins.Count >= resource.renewPrice;
+
+		private void Renew(GameEntity resource)
+			=> resource.Do((e) => e.UpdateResourceCurrentValue(e.progressBar.Value.Max))
+			           .Do((e) => _contexts.game.inventoryEntity.DecreaseCoinsCount(e.renewPrice));
 	}
 }
