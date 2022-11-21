6b9bcd3863ec633a6e79c489c16525cbb25bc7f5
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-21 03:28:05 +0300

    +LevelButton

---
+++ src/EcoFarm/Assets/Code/Menus/LevelSelectorScreen/LevelButton.cs
@@ -0,0 +1,17 @@
+﻿using Code.Unity.CustomTypes;
+using UnityEngine;
+
+namespace Code.Menus.LevelSelectorScreen
+{
+	public class LevelButton : UnityEventAdapter
+	{
+		[SerializeField] private int _levelIndex;
+		[SerializeField] private bool _isCompleted;
+		[SerializeField] private SceneField _sceneField;
+
+		protected override void OnButtonClick()
+		{
+			Debug.Log($"Level {_levelIndex} clicked");
+		}
+	}
+}
---
+++ src/EcoFarm/Assets/Code/Menus/UnityEventAdapter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.Menus
+{
+	public abstract class UnityEventAdapter : MonoBehaviour
+	{
+		[SerializeField] private Button _button;
+
+		private void OnEnable() => _button.onClick.AddListener(OnButtonClick);
+		
+		private void OnDisable() => _button.onClick.RemoveListener(OnButtonClick);
+
+		protected abstract void OnButtonClick();
+	}
+}
