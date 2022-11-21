c700bf811fa708dac9dbad049ce82bc6422986ca
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-21 03:31:48 +0300

    Component Added To Prefab

--- src/EcoFarm/Assets/Code/Menus/LevelSelectorScreen/LevelButton.cs
+++ src/EcoFarm/Assets/Code/Menus/LevelSelectorScreen/LevelButton.cs
@@ -1,17 +1,20 @@
-﻿using Code.Unity.CustomTypes;
+﻿using Code.Menus;
+using Code.Unity.CustomTypes;
 using UnityEngine;
 
-namespace Code.Menus.LevelSelectorScreen
+namespace EcoFarm.Code.Menus.LevelSelectorScreen
 {
 	public class LevelButton : UnityEventAdapter
 	{
 		[SerializeField] private int _levelIndex;
-		[SerializeField] private bool _isCompleted;
 		[SerializeField] private SceneField _sceneField;
+		[SerializeField] private bool _isCompleted;
+		[SerializeField] private GameObject _completedView;
 
-		protected override void OnButtonClick()
-		{
-			Debug.Log($"Level {_levelIndex} clicked");
-		}
+		protected override void OnButtonClick() => Debug.Log($"Level {_levelIndex} clicked");
+
+		private void OnValidate() => ActualizeCompleted();
+
+		private void ActualizeCompleted() => _completedView.SetActive(_isCompleted);
 	}
 }
