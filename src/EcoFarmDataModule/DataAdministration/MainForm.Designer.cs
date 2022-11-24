e2c9eec9cfef80c5caaa1dbd900baa94261905b9
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-25 00:39:51 +0300

    Created Test Code

--- src/EcoFarmDataModule/DataAdministration/MainForm.Designer.cs
+++ src/EcoFarmDataModule/DataAdministration/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DataAdministration
 {
-	partial class Form1
+	partial class MainForm
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -29,17 +29,33 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.ButtonTest = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
-			// Form1
+			// ButtonTest
+			// 
+			this.ButtonTest.Location = new System.Drawing.Point(227, 75);
+			this.ButtonTest.Name = "ButtonTest";
+			this.ButtonTest.Size = new System.Drawing.Size(114, 33);
+			this.ButtonTest.TabIndex = 0;
+			this.ButtonTest.Text = "Test";
+			this.ButtonTest.UseVisualStyleBackColor = true;
+			this.ButtonTest.Click += new System.EventHandler(this.ButtonTest_Click);
+			// 
+			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScroll = true;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Name = "Form1";
+			this.Controls.Add(this.ButtonTest);
+			this.Name = "MainForm";
 			this.Text = "Эко-ферма Модуль Управления Данными";
+			this.Load += new System.EventHandler(this.MainForm_Load);
 			this.ResumeLayout(false);
 		}
+
+		private System.Windows.Forms.Button ButtonTest;
 
 #endregion
 	}
---
+++ src/EcoFarmDataModule/DataAdministration/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using SQLite;
+
+namespace DataAdministration
+{
+	public partial class MainForm : Form
+	{
+		public MainForm() => InitializeComponent();
+
+		private static string Path => System.IO.Path.Combine(Directory.GetCurrentDirectory(), @"\EcoFarm.db");
+
+		private void MainForm_Load(object sender, EventArgs e) => CreateDbIfIsNotExists();
+
+		private void CreateDbIfIsNotExists()
+		{
+			if (File.Exists(Path))
+			{
+				return;
+			}
+
+			using (var dataBase = new SQLiteConnection(Path))
+			{
+				// dataBase.CreateTable<DevelopmentObject>();
+			}
+		}
+
+		private void ButtonTest_Click(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
