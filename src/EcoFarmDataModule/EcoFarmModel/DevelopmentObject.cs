af6734ebff33fdb2a23ac1e192bd2833f1e96954
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-25 00:42:58 +0300

    Created New Library on Framework

---
+++ src/EcoFarmDataModule/EcoFarmModel/DevelopmentObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SQLite;
+
+namespace EcoFarmDataModule
+{
+	[Serializable]
+	public abstract class DevelopmentObject
+	{
+		[PrimaryKey, AutoIncrement, Unique] public int    Id          { get; set; }
+		public                                     string Title       { get; set; }
+		public                                     string Description { get; set; }
+		public                                     int    Price       { get; set; }
+
+		public DevelopmentObject() { }
+
+		protected DevelopmentObject(string title, string description, int price)
+		{
+			Title = title;
+			Description = description;
+			Price = price;
+		}
+	}
+}
