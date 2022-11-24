239b5962ca4b8d7f1f4da47ebd96b1a28fc920bb
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-25 01:43:45 +0300

    Cleanup Model

--- src/EcoFarmDataModule/EcoFarmModel/DevelopmentObject.cs
+++ src/EcoFarmDataModule/EcoFarmModel/DevelopmentObject.cs
@@ -1,23 +1,12 @@
 using System;
-using SQLite;
 
 namespace EcoFarmModel
 {
 	[Serializable]
 	public abstract class DevelopmentObject
 	{
-		[PrimaryKey, AutoIncrement, Unique] public int    Id          { get; set; }
-		public                                     string Title       { get; set; }
-		public                                     string Description { get; set; }
-		public                                     int    Price       { get; set; }
-
-		public DevelopmentObject() { }
-
-		protected DevelopmentObject(string title, string description, int price)
-		{
-			Title = title;
-			Description = description;
-			Price = price;
-		}
+		public string Title       { get; set; }
+		public string Description { get; set; }
+		public int    Price       { get; set; }
 	}
 }
