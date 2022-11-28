1b661c74a151b3a3a4e7bf2bde16537cdcd96625
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-28 17:02:28 +0300

    is working

---
+++ src/EcoFarm/Assets/Code/Unity/TEMP/GameResultMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using static Code.Global.PlayerContexts.CustomTypes.SessionResult;
+
+namespace Code.Unity.TEMP
+{
+	public class GameResultMenu : MonoBehaviour
+	{
+		[SerializeField] private GameObject _nextLevelButton;
+		[SerializeField] private GameObject _retryLevelButton;
+		
+		private void Start()
+		{
+			var isVictory = Contexts.sharedInstance.player.currentPlayerEntity.sessionResult.Value is Victory;
+			
+			_nextLevelButton.SetActive(isVictory);
+			_retryLevelButton.SetActive(isVictory == false);
+		}
+	}
+}
