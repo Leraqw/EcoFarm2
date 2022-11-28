ee272a482661fa1869a98710496c4d2c453898e8
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-28 16:47:42 +0300

    added next level button

---
+++ src/EcoFarm/Assets/Code/Menus/UiBehaviours/Buttons/ButtonToNextLevel.cs
@@ -0,0 +1,12 @@
+﻿namespace Code.Menus.UiBehaviours.Buttons
+{
+	public class ButtonToNextLevel : ButtonToGameplay
+	{
+		protected override void OnButtonClick()
+		{
+			Contexts.sharedInstance.player.currentPlayerEntity.selectedLevel.Value++;
+			
+			base.OnButtonClick();
+		}
+	}
+}
