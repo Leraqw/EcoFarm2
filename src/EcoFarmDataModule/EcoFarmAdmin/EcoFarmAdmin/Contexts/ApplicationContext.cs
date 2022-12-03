e985ff75a7f0b7e6a14d6acb70f14e03febfd2a4
Rapuška <mikalajsiatkouski@gmail.com>
2022-12-03 23:08:36 +0300

    +ctor for AppContext

--- src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/Contexts/ApplicationContext.cs
+++ src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/Contexts/ApplicationContext.cs
@@ -4,6 +4,10 @@ namespace EcoFarmAdmin;
 
 public class ApplicationContext : DbContext
 {
+	private readonly string _path;
+
+	public ApplicationContext(string path) => _path = path;
+
 	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-		=> optionsBuilder.UseSqlite("Data Source = test.db");
+		=> optionsBuilder.UseSqlite($"Data Source = {_path}.db");
 }
