0d22e5b615c1b9c043fcc2bcf698414db76dd5b3
Rapuška <mikalajsiatkouski@gmail.com>
2022-12-05 01:43:54 +0300

    +ProductEditViewModel

---
+++ src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/ViewModels/EditWindows/ProductEditViewModel.cs
@@ -0,0 +1,19 @@
+﻿using DevExpress.Mvvm;
+using EcoFarmAdmin.Domain;
+
+namespace EcoFarmAdmin.ViewModels;
+
+public class ProductEditViewModel : ViewModelBase
+{
+	private DevObject _devObject;
+
+	public DevObject DevObject
+	{
+		get => _devObject;
+		set
+		{
+			_devObject = value;
+			RaisePropertiesChanged();
+		}
+	}
+}
