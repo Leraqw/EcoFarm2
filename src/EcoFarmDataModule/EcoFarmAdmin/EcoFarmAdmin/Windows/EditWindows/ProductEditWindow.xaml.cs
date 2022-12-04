849b172bb202facf351ad94e1ffb08628a9ac39d
Rapuška <mikalajsiatkouski@gmail.com>
2022-12-05 01:17:40 +0300

    +ProductEditWindow

---
+++ src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/Windows/EditWindows/ProductEditWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace EcoFarmAdmin.EditWindows;
+
+public partial class ProductEditWindow : Window
+{
+	public ProductEditWindow()
+	{
+		InitializeComponent();
+	}
+}
