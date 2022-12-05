b20820f279fc674238f41e7f9ecb29898978fa93
Rapuška <mikalajsiatkouski@gmail.com>
2022-12-05 13:46:58 +0300

    +Edit via button

--- src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/ViewModels/DataBaseViewModel.cs
+++ src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/ViewModels/DataBaseViewModel.cs
@@ -6,10 +6,20 @@ namespace EcoFarmAdmin.ViewModels;
 
 public class DataBaseViewModel : ViewModelBase
 {
+	public Product? SelectedProduct { get; set; }
+	
 	public ObservableCollection<Product> Products
 		=> DataBaseConnection.CurrentContext.Products.Local.ToObservableCollection();
 
 	public ICommand<object> AddProduct => new DelegateCommand(DataEditModel.AddProduct);
 
-	public ICommand<Product> EditProduct => new DelegateCommand<Product>((p) => DataEditModel.EditProduct(ref p));
+	public ICommand<Product> EditProduct
+		=> new DelegateCommand<Product>((p) => DataEditModel.EditProduct(ref p), (p) => p is not null);
+
+	public ICommand<Product> ButtonEditProduct
+		=> new DelegateCommand<Product>
+		(
+			(p) => DataEditModel.EditProduct(ref p),
+			(p) => p is not null
+		);
 }
