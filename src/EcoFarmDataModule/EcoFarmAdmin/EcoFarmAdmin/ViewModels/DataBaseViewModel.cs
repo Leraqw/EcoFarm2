8761f722765a22727f53abc49e9ac47043a78e8e
Rapuška <mikalajsiatkouski@gmail.com>
2022-12-05 13:06:54 +0300

    +AddProduct Commang

--- src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/ViewModels/DataBaseViewModel.cs
+++ src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/ViewModels/DataBaseViewModel.cs
@@ -9,6 +9,7 @@ public class DataBaseViewModel : ViewModelBase
 	public ObservableCollection<Product> Products
 		=> DataBaseConnection.CurrentContext.Products.Local.ToObservableCollection();
 
-	public ICommand<Product> EditDevObject
-		=> new DelegateCommand<Product>((p) => DataEditModel.EditProduct(ref p));
+	public ICommand<object> AddProduct => new DelegateCommand(DataEditModel.AddProduct);
+
+	public ICommand<Product> EditProduct => new DelegateCommand<Product>((p) => DataEditModel.EditProduct(ref p));
 }
