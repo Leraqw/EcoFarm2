027e1b4d5c3c3dab8b58d74e1ee5164b1fb9518f
Rapuška <mikalajsiatkouski@gmail.com>
2022-12-07 17:04:23 +0300

    +DevObjectOnLevelStartup class

---
+++ src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/Domain/DevObjectOnLevelStartup.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EcoFarmAdmin.Domain;
+
+[PrimaryKey(nameof(DevObjectID), nameof(LevelID))]
+public class DevObjectOnLevelStartup
+{
+	public int DevObjectID { get; set; }
+	public int LevelID     { get; set; }
+	public int Quantity    { get; set; }
+}
