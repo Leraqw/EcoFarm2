e7f2f84f6aced262a7d4f3fea061aba1df32c4e6
Rapuška <mikalajsiatkouski@gmail.com>
2022-12-07 03:01:39 +0300

    +LevelVM methods

---
+++ src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/ViewModels/LevelsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DevExpress.Mvvm;
+using EcoFarmAdmin.Domain;
+
+namespace EcoFarmAdmin.ViewModels;
+
+public class LevelsViewModel : TableViewModel<Level>
+{
+	public ICommand<Level> MoveUp   => new DelegateCommand<Level>(MoveLevelUp);
+	public ICommand<Level> MoveDown => new DelegateCommand<Level>(MoveLevelDown);
+
+	private void MoveLevelUp(Level level) { }
+
+	private void MoveLevelDown(Level level) { }
+}
