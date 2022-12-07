1087c63802e68ff7cd249e2a8a62f6da588c018a
Rapuška <mikalajsiatkouski@gmail.com>
2022-12-07 03:27:07 +0300

    functioanl-like refactoring

--- src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/ViewModels/LevelsViewModel.cs
+++ src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/ViewModels/LevelsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using DevExpress.Mvvm;
 using DevExpress.Mvvm.Native;
 using EcoFarmAdmin.Domain;
@@ -10,30 +12,21 @@ public class LevelsViewModel : TableViewModel<Level>
 	public ICommand<Level> MoveUp   => new DelegateCommand<Level>(MoveLevelUp);
 	public ICommand<Level> MoveDown => new DelegateCommand<Level>(MoveLevelDown);
 
-	private void MoveLevelUp(Level level)
+	private void MoveLevelUp(Level level) => MoveLevel(level, isAtBorder: (i, _) => i <= 0, step: (i) => i - 1);
+
+	private void MoveLevelDown(Level level)
+		=> MoveLevel(level, isAtBorder: (i, list) => i >= list.Count - 1, step: (i) => i + 1);
+
+	private void MoveLevel(Level level, Func<int, List<Level>, bool> isAtBorder, Func<int, int> step)
 	{
 		var sortedLevels = Collection.OrderBy((l) => l.Order).ToList();
 		var indexOfCurrent = sortedLevels.IndexOf((l) => l.Id == level.Id);
-		if (indexOfCurrent <= 0)
+		if (isAtBorder(indexOfCurrent, sortedLevels))
 		{
 			return;
 		}
 
-		var levelToSwap = sortedLevels[indexOfCurrent - 1];
-		(levelToSwap.Order, level.Order) = (level.Order, levelToSwap.Order);
-		Refresh();
-	}
-
-	private void MoveLevelDown(Level level)
-	{ 
-		var sortedLevels = Collection.OrderBy((l) => l.Order).ToList();
-		var indexOfCurrent = sortedLevels.IndexOf((l) => l.Id == level.Id);
-		if (indexOfCurrent >= sortedLevels.Count - 1)
-		{
-			return;
-		}
-
-		var levelToSwap = sortedLevels[indexOfCurrent + 1];
+		var levelToSwap = sortedLevels[step(indexOfCurrent)];
 		(levelToSwap.Order, level.Order) = (level.Order, levelToSwap.Order);
 		Refresh();
 	}
