22226b179aa855e5608922cb9fb633fce3676cf1
Rapuška <mikalajsiatkouski@gmail.com>
2022-12-08 13:49:27 +0300

    +other controls behaviour

--- src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/ViewModels/LevelsWindow/LevelsGoalsViewModel.cs
+++ src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/ViewModels/LevelsWindow/LevelsGoalsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Linq;
+using DevExpress.Mvvm;
 using DevExpress.Mvvm.Native;
 using EcoFarmAdmin.Domain;
 
@@ -7,14 +8,41 @@ namespace EcoFarmAdmin.ViewModels;
 
 public class LevelsGoalsViewModel : DevObjectsOnLevelsViewModel
 {
+	public Goal? SelectedGoal { get; set; }
+
 	public ObservableCollection<Goal>? GoalsOnSelectedLevel { get; set; }
+	public ICommand<object>            AddGoalCommand       => new DelegateCommand(AddGoal);
+
+	public ICommand<Goal> DeleteGoalCommand
+		=> new DelegateCommand<Goal>
+		(
+			DeleteGoal,
+			(_) => IsDosSelected
+		);
+
+	public bool IsDosSelected => SelectedDosOnStart != null;
+
+	private void AddGoal()
+	{
+		var newGoal = new Goal { Level = SelectedItem! };
+		Goals.Add(newGoal);
+		OnSelectionChanged(SelectedItem!);
+		Refresh();
+	}
+
+	private void DeleteGoal(Goal goal)
+	{
+		Goals.Remove(goal);
+		OnSelectionChanged(SelectedItem!);
+		Refresh();
+	}
 
 	protected override void OnSelectionChanged(Level level)
 	{
 		GoalsOnSelectedLevel = Goals
 		                       .Where((g) => g.Level == level)
 		                       .ToObservableCollection();
-		
+
 		base.OnSelectionChanged(level);
 	}
 }
