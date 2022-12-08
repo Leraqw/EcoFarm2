8bcd6bc53aab1eefe8cce702a2b440ad2ea09e81
Rapuška <mikalajsiatkouski@gmail.com>
2022-12-08 18:46:10 +0300

    +migrations

---
+++ src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/Migrations/20221208154532_AddedInputOutputProducts.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcoFarmAdmin.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedInputOutputProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InputProducts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FactoryId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProductId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InputProducts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InputProducts_Factories_FactoryId",
+                        column: x => x.FactoryId,
+                        principalTable: "Factories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_InputProducts_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OutputProducts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FactoryId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProductId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutputProducts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OutputProducts_Factories_FactoryId",
+                        column: x => x.FactoryId,
+                        principalTable: "Factories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OutputProducts_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InputProducts_FactoryId",
+                table: "InputProducts",
+                column: "FactoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InputProducts_ProductId",
+                table: "InputProducts",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OutputProducts_FactoryId",
+                table: "OutputProducts",
+                column: "FactoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OutputProducts_ProductId",
+                table: "OutputProducts",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InputProducts");
+
+            migrationBuilder.DropTable(
+                name: "OutputProducts");
+        }
+    }
+}
