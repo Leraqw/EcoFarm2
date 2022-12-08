ef4ac73cb216d5164e03ab07a15d8c1acefad3b7
Rapuška <mikalajsiatkouski@gmail.com>
2022-12-08 18:29:40 +0300

    duplicated vms

---
+++ src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/ViewModels/FactoriesWindow/InputObjectsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using DevExpress.Mvvm;
+using DevExpress.Mvvm.Native;
+using EcoFarmAdmin.Domain;
+
+namespace EcoFarmAdmin.ViewModels;
+
+public class InputObjectsViewModel : TableViewModel<Level>
+{
+	public DevObjectOnLevelStartup? SelectedDosOnStart { get; set; }
+	public ObservableCollection<DevObjectOnLevelStartup>? DevObjectOnSelectedLevel { get; set; }
+	public ICommand<Level> OnSelectionChangedCommand => new DelegateCommand<Level>(OnSelectionChanged);
+	public ICommand<object> AddDosOnStart => new DelegateCommand(AddDevObjectOnStart);
+
+	public ICommand<DevObjectOnLevelStartup> DeleteDosOnStart
+		=> new DelegateCommand<DevObjectOnLevelStartup>
+		(
+			DeleteDevObjectOnStart,
+			(_) => IsDosSelected
+		);
+
+	public bool IsDosSelected => SelectedDosOnStart != null;
+
+	private void AddDevObjectOnStart()
+	{
+		var newDl = new DevObjectOnLevelStartup { Level = SelectedItem! };
+		DevObjectOnLevelsStartup.Add(newDl);
+		OnSelectionChanged(SelectedItem!);
+		Refresh();
+	}
+
+	private void DeleteDevObjectOnStart(DevObjectOnLevelStartup devObjectOnLevelStartup)
+	{
+		DevObjectOnLevelsStartup.Remove(devObjectOnLevelStartup);
+		OnSelectionChanged(SelectedItem!);
+		Refresh();
+	}
+
+	protected virtual void OnSelectionChanged(Level level)
+		=> DevObjectOnSelectedLevel = DevObjectOnLevelsStartup
+		                              .Where((dl) => dl.Level == level)
+		                              .ToObservableCollection();
+}
---
+++ src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/ViewModels/FactoriesWindow/OutputProductsViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using DevExpress.Mvvm;
+using DevExpress.Mvvm.Native;
+using EcoFarmAdmin.Domain;
+
+namespace EcoFarmAdmin.ViewModels;
+
+public class OutputProductsViewModel : DevObjectsOnLevelsViewModel
+{
+	public Goal? SelectedGoal { get; set; }
+
+	public ObservableCollection<Goal>? GoalsOnSelectedLevel { get; set; }
+	public ICommand<object>            AddGoalCommand       => new DelegateCommand(AddGoal);
+
+	public ICommand<Goal> DeleteGoalCommand
+		=> new DelegateCommand<Goal>
+		(
+			DeleteGoal,
+			(_) => IsGoalSelected
+		);
+
+	public bool IsGoalSelected => SelectedGoal != null;
+
+	private void AddGoal()
+	{
+		var newGoal = new Goal { Level = SelectedItem! };
+		Goals.Add(newGoal);
+		OnSelectionChanged(SelectedItem!);
+		Refresh();
+	}
+
+	private void DeleteGoal(Goal goal)
+	{
+		Goals.Remove(goal);
+		OnSelectionChanged(SelectedItem!);
+		Refresh();
+	}
+
+	protected override void OnSelectionChanged(Level level)
+	{
+		GoalsOnSelectedLevel = Goals
+		                       .Where((g) => g.Level == level)
+		                       .ToObservableCollection();
+
+		base.OnSelectionChanged(level);
+	}
+}
