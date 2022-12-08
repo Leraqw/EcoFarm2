c40f957f3af70e67aa0198efe59bf7e82591d1db
Rapuška <mikalajsiatkouski@gmail.com>
2022-12-08 12:31:59 +0300

    extracted DevObjectsOnLevelsViewModel

--- src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/ViewModels/LevelsViewModel.cs
+++ src/EcoFarmDataModule/EcoFarmAdmin/EcoFarmAdmin/ViewModels/LevelsViewModel.cs
@@ -1,64 +1,8 @@
-﻿using System.Collections.ObjectModel;
-using System.Linq;
-using DevExpress.Mvvm;
-using DevExpress.Mvvm.Native;
-using EcoFarmAdmin.Domain;
+﻿using System.Linq;
 
 namespace EcoFarmAdmin.ViewModels;
 
-public class LevelsViewModel : LevelsMoverViewModel
+public class LevelsViewModel : DevObjectsOnLevelsViewModel
 {
 	public LevelsViewModel() => OnSelectionChanged(Collection.First());
-
-	public DevObjectOnLevelStartup?                       SelectedDosOnStart       { get; set; }
-	public ObservableCollection<DevObjectOnLevelStartup>? DevObjectOnSelectedLevel { get; set; }
-
-	public ICommand<Level> OnSelectionChangedCommand => new DelegateCommand<Level>(OnSelectionChanged);
-
-	public ICommand<object> AddDosOnStart => new DelegateCommand(AddDevObjectOnStart);
-
-	public ICommand<DevObjectOnLevelStartup> DeleteDosOnStart
-		=> new DelegateCommand<DevObjectOnLevelStartup>
-		(
-			DeleteDevObjectOnStart,
-			(_) => IsDosSelected
-		);
-
-	public bool IsDosSelected => SelectedDosOnStart != null;
-
-	protected override void AddItem()
-	{
-		base.AddItem();
-
-		var item = Collection.Last();
-		item.Order = Collection.Max((l) => l.Order) + 1;
-		item.Id = Collection.Max((l) => l.Id) + 1;
-	}
-
-	protected override void DeleteItem(Level item)
-	{
-		base.DeleteItem(item);
-
-		Collection.Where((l) => l.Order > item.Order).ForEach((l) => l.Order--);
-	}
-
-	private void AddDevObjectOnStart()
-	{
-		var newDl = new DevObjectOnLevelStartup { Level = SelectedItem! };
-		DevObjectOnLevelsStartup.Add(newDl);
-		OnSelectionChanged(SelectedItem!);
-		Refresh();
-	}
-
-	private void DeleteDevObjectOnStart(DevObjectOnLevelStartup devObjectOnLevelStartup)
-	{
-		DevObjectOnLevelsStartup.Remove(devObjectOnLevelStartup);
-		OnSelectionChanged(SelectedItem!);
-		Refresh();
-	}
-
-	private void OnSelectionChanged(Level level)
-		=> DevObjectOnSelectedLevel = DevObjectOnLevelsStartup
-		                              .Where((dl) => dl.Level == level)
-		                              .ToObservableCollection();
 }
