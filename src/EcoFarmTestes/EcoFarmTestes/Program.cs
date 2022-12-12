95c3ee26769f493f7266333423d17288da140505
Rapuška <mikalajsiatkouski@gmail.com>
2022-12-12 22:39:25 +0300

    incompleted try to test TreeWatering

--- src/EcoFarmTestes/EcoFarmTestes/Program.cs
+++ src/EcoFarmTestes/EcoFarmTestes/Program.cs
@@ -1,10 +1,43 @@
-﻿using Xunit;
+﻿using Code.Utils.Extensions;
+using Code.Utils.Extensions.Entitas;
+using UnityEngine;
+using Xunit;
 
-public class TestTests
+public class TreeWateringTests
 {
-	[Fact]
-	public void SuccessfulTest() => Assert.Equal(1, 1);
+	private GameEntity _tree;
+	private GameEntity _bucket;
+	private GameContext _context;
+
+	public TreeWateringTests()
+	{
+		_context = new GameContext();
+		_tree = InitializeTree(_context);
+		_bucket = InitializeBucket(_context);
+	}
+
+	private GameEntity InitializeTree(GameContext context)
+	{
+		var entity = context.CreateEntity();
+		entity.Do((e) => e.AddDebugName("Tree"))
+		     .Do((e) => e.MakeAttachable())
+		     .Do((e) => e.AddSpawnPosition(e.requireTreeOnPosition))
+		     .Do((e) => e.isFruitful = true)
+		     .Do((e) => e.RemoveRequireTreeOnPosition());
+		return entity;
+	}
+
+	private GameEntity InitializeBucket(GameContext context)
+	{
+		var entity = context.CreateEntity();
+		entity.AddRadius(2);
+		entity.AddPosition(Vector2.zero);
+		return entity;
+	}
 
 	[Fact]
-	public void FailureTest() => Assert.Equal(1, 2);
+	public void BucketDroppedNearTree()
+	{
+		
+	}
 }
