dd588d193f9e96eb5948600235614212a91ed582
Rapuška <mikalajsiatkouski@gmail.com>
2022-12-13 19:29:04 +0300

    +2 simple tests

---
+++ src/EcoFarm/Assets/Code/Tests/Create.cs
@@ -0,0 +1,12 @@
+﻿using Code.ECS.Systems.Warehouse;
+
+namespace Code.Tests
+{
+	public class Create
+	{
+		public static PickedToWarehouseSystem System(Contexts contexts)
+		{
+			return new PickedToWarehouseSystem(contexts);
+		}
+	}
+}
