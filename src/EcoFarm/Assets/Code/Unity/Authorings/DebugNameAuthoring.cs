1d8bfd40c39376fdf8b734d2c57794275b8fec1a
Rapuška <mikalajsiatkouski@gmail.com>
2022-12-15 11:18:01 +0300

    DebugNameAuthoring

---
+++ src/EcoFarm/Assets/Code/Unity/Authorings/DebugNameAuthoring.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Code
+{
+	public class DebugNameAuthoring : RegistrarBase
+	{
+		[SerializeField] private string _value;
+
+		public override void Register(ref GameEntity entity)
+		{
+			entity.AddDebugName(_value);
+		}
+	}
+}
---
+++ src/EcoFarm/Assets/Code/Unity/Authorings/RegistrableEntityBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Code
+{
+	public class RegistrableEntityBehaviour : MonoBehaviour
+	{
+		[SerializeField] private RegistrarBase[] _registrars;
+
+		private void Awake()
+		{
+			var entity = Contexts.sharedInstance.game.CreateEntity();
+
+			foreach (var registrar in _registrars)
+			{
+				registrar.Register(ref entity);
+			}
+		}
+	}
+}
