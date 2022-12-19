d08a0acc1ffb3cfda951b7b9964016b4344408a3
Rapuška <mikalajsiatkouski@gmail.com>
2022-12-19 11:17:06 +0300

    +DependenciesAttribute

---
+++ src/EcoFarmCustomGenerator/EcoFarmCustomGenerator.CodeGeneration.Attributes/DependenciesAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EcoFarmCustomGenerator.CodeGeneration.Attributes
+{
+	[AttributeUsage(AttributeTargets.Class)]
+	public class DependenciesAttribute : Attribute
+	{
+		private readonly Type[] _dependencies;
+		public List<Type> Dependencies => new List<Type>(_dependencies);
+
+		public DependenciesAttribute(params Type[] dependencies) => _dependencies = dependencies;
+	}
+}
