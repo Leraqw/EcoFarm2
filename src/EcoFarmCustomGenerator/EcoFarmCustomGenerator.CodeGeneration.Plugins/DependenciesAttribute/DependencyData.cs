1a293b3a4c95c2a2c3d3e7705f7488bf8c376adb
Rapuška <mikalajsiatkouski@gmail.com>
2022-12-19 11:22:34 +0300

    +DependencyData

---
+++ src/EcoFarmCustomGenerator/EcoFarmCustomGenerator.CodeGeneration.Plugins/DependenciesAttribute/DependencyData.cs
@@ -0,0 +1,29 @@
+﻿using DesperateDevs.CodeGeneration;
+
+namespace EcoFarmCustomGenerator.CodeGeneration.Plugins
+{
+	public class DependencyData : CodeGeneratorData
+	{
+		public const string NameKey = "Dependency.Name";
+		public const string MemberKey = "Dependency.Members";
+		public const string DependenciesKey = "Dependency.Dependencies";
+
+		public string Name
+		{
+			get => (string)this[NameKey];
+			set => this[NameKey] = value;
+		}
+
+		public string Members
+		{
+			get => (string)this[MemberKey];
+			set => this[MemberKey] = value;
+		}
+
+		public string Dependencies
+		{
+			get => (string)this[DependenciesKey];
+			set => this[DependenciesKey] = value;
+		}
+	}
+}
