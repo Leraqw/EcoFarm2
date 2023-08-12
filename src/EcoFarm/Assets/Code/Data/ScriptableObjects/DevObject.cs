c7f1758c84294e57b5874009a3a5132c6a66686b
rapushka <mikalajsiatkouski@gmail.com>
2023-08-12 18:12:44 +0300

    added sprite to dev objects

--- src/EcoFarm/Assets/Code/Data/ScriptableObjects/DevObject.cs
+++ src/EcoFarm/Assets/Code/Data/ScriptableObjects/DevObject.cs
@@ -7,5 +7,6 @@ namespace EcoFarm
 		[field: SerializeField] public string Title       { get; private set; }
 		[field: SerializeField] public string Description { get; private set; }
 		[field: SerializeField] public int    Price       { get; private set; }
+		[field: SerializeField] public Sprite Sprite      { get; private set; }
 	}
 }
