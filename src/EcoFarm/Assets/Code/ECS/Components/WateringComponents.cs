24d3856e3493d9bdea54426b1465cff978bc18cc
rapushka <mikalajsiatkouski@gmail.com>
2023-08-13 17:28:58 +0300

    removed custom code generator

--- src/EcoFarm/Assets/Code/ECS/Components/WateringComponents.cs
+++ src/EcoFarm/Assets/Code/ECS/Components/WateringComponents.cs
@@ -1,10 +1,9 @@
-﻿using EcoFarmCustomGenerator.CodeGeneration.Attributes;
-using Entitas;
+﻿using Entitas;
 
 namespace EcoFarm
 {
 	[Game] public sealed class FilledComponent : IComponent { }
 	[Game] public sealed class RadiusComponent : IComponent { public float Value; }
-	[Game] [Cool] public sealed class CraneComponent : IComponent { }
+	[Game] public sealed class CraneComponent : IComponent { }
 	[Game] public sealed class BucketComponent : IComponent { }
 }
