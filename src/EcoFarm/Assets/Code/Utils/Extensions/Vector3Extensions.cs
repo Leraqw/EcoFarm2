573a62b51b0306f6f2c3e0c11cc0ffab2a50d872
rapushka <mikalajsiatkouski@gmail.com>
2023-08-13 17:39:02 +0300

    removed Libraries folder

--- src/EcoFarm/Assets/Code/Utils/Extensions/Vector3Extensions.cs
+++ src/EcoFarm/Assets/Code/Utils/Extensions/Vector3Extensions.cs
@@ -6,12 +6,12 @@ namespace EcoFarm
 	{
 		public static int Compare(this Vector3 @this, Vector3 other) => @this.IsGreater(other) ? 1 : -1;
 
-		public static bool IsGreater(this Vector3 @this, Vector3 other) 
+		public static bool IsGreater(this Vector3 @this, Vector3 other)
 			=> @this.x > other.x || @this.y > other.y || @this.z > other.z;
-		
+
 		public static bool IsLess(this Vector3 @this, Vector3 other)
 			=> @this.x < other.x || @this.y < other.y || @this.z < other.z;
-		
+
 		public static float Avg(this Vector3 @this) => (@this.x + @this.y + @this.z) / 3;
 	}
 }
