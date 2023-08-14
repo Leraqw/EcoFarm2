7fdadab52d8954153a93c869243ec85769a87157
Lera <valeriaseskova@gmail.com>
2023-08-14 18:05:37 +0300

    fixed factory bug (continued working when theres no energy)

--- src/EcoFarm/Assets/Code/ECS/Systems/Buildings/Factories/ClickOnFactorySystem.cs
+++ src/EcoFarm/Assets/Code/ECS/Systems/Buildings/Factories/ClickOnFactorySystem.cs
@@ -5,64 +5,62 @@ using static GameMatcher;
 
 namespace EcoFarm
 {
-	public sealed class ClickOnFactorySystem : ReactiveSystem<GameEntity>
-	{
-		private readonly Contexts _contexts;
+    public sealed class ClickOnFactorySystem : ReactiveSystem<GameEntity>
+    {
+        private readonly Contexts _contexts;
 
-		private Dictionary<Product, int> _requiredProducts;
+        private Dictionary<Product, int> _requiredProducts;
 
-		public ClickOnFactorySystem(Contexts contexts) : base(contexts.game) => _contexts = contexts;
+        public ClickOnFactorySystem(Contexts contexts) : base(contexts.game) => _contexts = contexts;
 
-		private Dictionary<Product, int> AvailableProducts
-			=> _contexts.game.GetInventoryItems()
-			            .ToDictionary((i) => i.product.Value, (i) => i.inventoryItem.Value.Count);
+        private Dictionary<Product, int> AvailableProducts
+            => _contexts.game.GetInventoryItems()
+                .ToDictionary((i) => i.product.Value, (i) => i.inventoryItem.Value.Count);
 
-		protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
-			=> context.CreateCollector(AllOf(GameMatcher.Factory, MouseDown).NoneOf(Busy));
+        protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
+            => context.CreateCollector(AllOf(GameMatcher.Factory, MouseDown).NoneOf(Busy));
 
-		protected override bool Filter(GameEntity entity) => true;
+        protected override bool Filter(GameEntity entity) => true;
 
-		protected override void Execute(List<GameEntity> entites) => entites.ForEach(Handle);
+        protected override void Execute(List<GameEntity> entites) => entites.ForEach(Handle);
 
-		private void Handle(GameEntity factory)
-			=> factory
-			   .Do((e) => _requiredProducts = e.RequiredProducts())
-			   .Do(TakeProducts, @if: IsEnoughOnWarehouse);
+        private void Handle(GameEntity factory)
+        {
+            _requiredProducts = factory.RequiredProducts();
+            factory.Do(TakeProducts, @if:IsEnoughResources);
+        }
 
-		private bool IsEnoughOnWarehouse(GameEntity factory)
-			=> _requiredProducts.All((p) => AvailableProducts[p.Key] >= p.Value);	
-		
-		// private bool IsEnoughEnergy(GameEntity factory)
-		// {
-		// 	IBalanceConfig Balance = _contexts.services.configurationService.Value.Balance;
-		// 	Balance.Energy;
-		// }
+        private bool IsEnoughOnWarehouse(GameEntity factory)
+            => _requiredProducts.All((p) => AvailableProducts[p.Key] >= p.Value);
 
-		private void TakeProducts(GameEntity factory)
-			=> factory
-			   .Do(MarkAsBusy)
-			   .Do(TakeEachRequiredProducts);
+        private bool IsEnoughResources(GameEntity factory) => factory.IsResourceEnough() && IsEnoughOnWarehouse(factory);
 
-		private static void MarkAsBusy(GameEntity entity) => entity.isBusy = true;
 
-		private void TakeEachRequiredProducts(GameEntity factory)
-			=> _requiredProducts.ForEach((p) => Take(p, factory));
+        private void TakeProducts(GameEntity factory)
+            => factory
+                .Do(MarkAsBusy)
+                .Do(TakeEachRequiredProducts);
 
-		private void Take(KeyValuePair<Product, int> product, GameEntity factory)
-		{
-			CreateRequest(product, factory);
-			DecreaseProductsCount(product);
-		}
+        private static void MarkAsBusy(GameEntity entity) => entity.isBusy = true;
 
-		private void CreateRequest(KeyValuePair<Product, int> product, GameEntity factory)
-			=> _contexts.game.CreateEntity()
-			            .Do((e) => e.AddRequireProduct(product.Key))
-			            .Do((e) => e.AddPosition(factory.position.Value))
-			            .Do((e) => e.AddCount(product.Value))
-			            .AttachTo(factory);
+        private void TakeEachRequiredProducts(GameEntity factory)
+            => _requiredProducts.ForEach((p) => Take(p, factory));
 
-		private void DecreaseProductsCount(KeyValuePair<Product, int> product)
-			=> _contexts.game.GetInventoryItem(product.Key)
-			            .DecreaseInventoryItemCount(product.Value);
-	}
+        private void Take(KeyValuePair<Product, int> product, GameEntity factory)
+        {
+            CreateRequest(product, factory);
+            DecreaseProductsCount(product);
+        }
+
+        private void CreateRequest(KeyValuePair<Product, int> product, GameEntity factory)
+            => _contexts.game.CreateEntity()
+                .Do((e) => e.AddRequireProduct(product.Key))
+                .Do((e) => e.AddPosition(factory.position.Value))
+                .Do((e) => e.AddCount(product.Value))
+                .AttachTo(factory);
+
+        private void DecreaseProductsCount(KeyValuePair<Product, int> product)
+            => _contexts.game.GetInventoryItem(product.Key)
+                .DecreaseInventoryItemCount(product.Value);
+    }
 }
