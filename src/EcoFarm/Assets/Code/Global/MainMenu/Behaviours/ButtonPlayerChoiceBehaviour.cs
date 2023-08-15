71fc7d8fb706e24b86365676ccc8b850f278eedf
Lera <valeriaseskova@gmail.com>
2023-08-15 17:00:49 +0300

    ButtonPlayerChoiceBehaviour, PlayerChoiceButton components

---
+++ src/EcoFarm/Assets/Code/Global/MainMenu/Behaviours/ButtonPlayerChoiceBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace EcoFarm
+{
+    public class ButtonPlayerChoiceBehaviour : StartEntityBehaviour
+    {
+        [SerializeField] private InteractableView _interactableView;
+
+        protected override void Initialize()
+        {
+            var e = Contexts.player.CreateEntity();
+            e.AddView(gameObject);
+            e.AddInteractable(true);
+            e.isPlayerChoiceButton = true;
+            e.isForPlayerButton = true;
+            _interactableView.Register(e);
+        }
+    }
+}
