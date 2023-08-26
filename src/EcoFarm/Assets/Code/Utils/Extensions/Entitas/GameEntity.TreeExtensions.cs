7f169b5df761dde12c067653654ae614819c8075
rapushka <mikalajsiatkouski@gmail.com>
2023-08-26 11:48:21 +0300

    removed false commentary

--- src/EcoFarm/Assets/Code/Utils/Extensions/Entitas/GameEntity.TreeExtensions.cs
+++ src/EcoFarm/Assets/Code/Utils/Extensions/Entitas/GameEntity.TreeExtensions.cs
@@ -3,9 +3,8 @@ using Zenject;
 
 public partial class GameEntity
 {
-	// ideally Zenject shouldn't reach here
-	// but you can mark fields with the [Inject] attribute just to make it clear that it's a container dependency.
-	[Inject] private IConfigurationService _configuration; 
+	/// <summary> Use for inject only! </summary>
+	[Inject] public IConfigurationService Configuration; 
 
-	public void ResetDroughtTimer() => AddDuration(_configuration.Balance.Watering.DroughtDuration);
+	public void ResetDroughtTimer() => AddDuration(Configuration.Balance.Watering.DroughtDuration);
 }
